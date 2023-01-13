9a7a583e93341ec9fbd6a1489fed5f5355575010
NaayoungKwon <kny8092@gmail.com>
2023-01-13 18:58:01 +0900

    setting : ci 확인을 위한 controller 설정 및 security disable

---
+++ src/main/java/io/eagle/wealthmarblebackend/test/CiTestController.java
@@ -0,0 +1,17 @@
+package io.eagle.wealthmarblebackend.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class CiTestController {
+
+    @GetMapping("/test")
+    public String test(){
+        System.out.println("${spring.datasource.username}");
+        return "spring ci test";
+    }
+
+}
