0d96b64a3ab3149edf101b39e69eec0d850a565d
NaayoungKwon <kny8092@gmail.com>
2023-01-17 00:24:06 +0900

    feature : 공모 entity foreign key 임시 생성

---
+++ src/main/java/io/eagle/wealthmarblebackend/domain/user/domain/User.java
@@ -0,0 +1,13 @@
+package io.eagle.wealthmarblebackend.domain.user.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
