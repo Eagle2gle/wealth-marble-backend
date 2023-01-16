36484542e91ccc5d2c356fa0b4a75c89f0dc4296
NaayoungKwon <kny8092@gmail.com>
2023-01-16 17:47:18 +0900

    feature : 생성, 수정 시간 base entity 작성

---
+++ src/main/java/io/eagle/wealthmarblebackend/common/BaseEntity.java
@@ -0,0 +1,21 @@
+package io.eagle.wealthmarblebackend.common;
+
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Data
+@MappedSuperclass
+@EntityListeners(value = AuditingEntityListener.class)
+public abstract class BaseEntity {
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
