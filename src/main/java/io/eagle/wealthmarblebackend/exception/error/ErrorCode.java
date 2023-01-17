458354ff3415815e44bd01afb61c3960340a31af
NaayoungKwon <kny8092@gmail.com>
2023-01-18 00:19:03 +0900

    feat/#14 : ErrorCode 초안 작성

---
+++ src/main/java/io/eagle/wealthmarblebackend/exception/error/ErrorCode.java
@@ -0,0 +1,34 @@
+package io.eagle.wealthmarblebackend.exception.error;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    // Global
+    INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "잘못된 파라미터입니다."),
+    INVALID_DTO(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 오류입니다."),
+
+    // User
+
+
+    // Vacation
+    VACATION_NOT_FOUND(HttpStatus.NOT_FOUND,"존재하지 않는 공모 ID 입니다"),
+
+    // Order
+
+
+    // Transaction
+
+
+    // Price Info
+
+
+    ;
+    private final HttpStatus httpStatus;
+    private final String message;
+}
