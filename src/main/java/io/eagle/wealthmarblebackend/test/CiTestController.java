bd691dcff18109b558b595897b4928a17df3823f
NaayoungKwon <kny8092@gmail.com>
2023-01-18 01:51:32 +0900

    chore/#14 : 예외처리 확인용 임시 controller 작성

--- src/main/java/io/eagle/wealthmarblebackend/test/CiTestController.java
+++ src/main/java/io/eagle/wealthmarblebackend/test/CiTestController.java
@@ -1,7 +1,10 @@
 package io.eagle.wealthmarblebackend.test;
 
+import io.eagle.wealthmarblebackend.exception.ApiException;
+import io.eagle.wealthmarblebackend.exception.error.ErrorCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -12,6 +15,17 @@ public class CiTestController {
     public String test(){
         System.out.println("${spring.datasource.username}");
         return "spring ci test";
+    }
+
+    @GetMapping("/exception/{id}")
+    public String testParamError(@PathVariable("id") Long id ){
+        return "param id : "+ id;
+    }
+
+    @GetMapping("/exception/api")
+    public String testApiError( ){
+        throw new ApiException(ErrorCode.VACATION_NOT_FOUND);
+//        return "pass";
     }
 
 }
