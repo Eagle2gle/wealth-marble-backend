baf1de363738531bbcebf3c846e401a8b8f06ce5
NaayoungKwon <kny8092@gmail.com>
2023-01-18 01:51:04 +0900

    chore/#14 : request dto 관련 error 처리 임시 주석 처리

--- src/main/java/io/eagle/wealthmarblebackend/exception/GlobalExceptionHandler.java
+++ src/main/java/io/eagle/wealthmarblebackend/exception/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package io.eagle.wealthmarblebackend.exception;
 import io.eagle.wealthmarblebackend.exception.error.ErrorCode;
 import io.eagle.wealthmarblebackend.exception.error.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
@@ -24,7 +27,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     * API 요청 runtime 에러가
     * */
     @ExceptionHandler(ApiException.class)
-    protected ResponseEntity<Object> handleCustomException(ApiException e) {
+    public ResponseEntity<Object> handleCustomException(ApiException e) {
         ErrorCode errorCode = e.getErrorCode();
         log.warn("{}", errorCode.getMessage() );
         return handleExceptionInternal(errorCode);
@@ -33,40 +36,61 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     /*
      * Request parameter가 올바르지 못한 경우 에러
      */
-    @ExceptionHandler(MissingServletRequestParameterException.class)
-    protected ResponseEntity<Object> handleMissingRequestParamException(Exception e, HttpServletRequest request) {
-        ErrorCode errorCode = ErrorCode.INVALID_PARAMETER;
-        log.warn("[{}] Request Parameter 이상..", request.getRequestURI() );
-        return handleExceptionInternal(errorCode);
-    }
+//    @ExceptionHandler(MissingServletRequestParameterException.class)
+//    protected ResponseEntity<Object> handleMissingRequestParamException(Exception e, HttpServletRequest request) {
+//        ErrorCode errorCode = ErrorCode.INVALID_PARAMETER;
+//        log.warn("[{}] Request Parameter 이상..", request.getRequestURI() );
+//        return handleExceptionInternal(errorCode);
+//    }
+//    @Override
+//    public ResponseEntity<Object> handleMissingServletRequestParameter(
+//            MissingServletRequestParameterException e,
+//            HttpHeaders headers,
+//            HttpStatus status,
+//            WebRequest request){
+//        ErrorCode errorCode = ErrorCode.INVALID_PARAMETER;
+//        log.warn("Request Parameter 이상..");
+//        return handleExceptionInternal(errorCode);
+//    }
 
     /*
      * Request parameter DTO 검증 에러
      */
-    @ExceptionHandler(BindException.class)
-    protected ResponseEntity<Object> handleRequestParamValidException(BindException e, HttpServletRequest request) {
-        ErrorCode errorCode = ErrorCode.INVALID_PARAMETER;
-        String message = makeBindingErrorMessage(e.getBindingResult());
-        log.warn("[{}] Request Parameter 이상.. {}", request.getRequestURI(), message );
-        return handleExceptionInternal(errorCode, message);
-    }
-
-    /*
-     * Request Body 검증 에러
-     */
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<Object> handleRequestBodyValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
-        ErrorCode errorCode = ErrorCode.INVALID_REQUEST_BODY;
-        String message = makeBindingErrorMessage(e.getBindingResult());
-        log.warn("[{}] Request Body 이상.. {}", request.getRequestURL(), message );
-        return handleExceptionInternal(errorCode, message);
-    }
+//    @ExceptionHandler(BindException.class)
+//    public ResponseEntity<Object> handleRequestParamValidException(BindException e, HttpServletRequest request) {
+//        ErrorCode errorCode = ErrorCode.INVALID_PARAMETER;
+//        String message = makeBindingErrorMessage(e.getBindingResult());
+//        log.warn("[{}] Request Parameter 이상.. {}", request.getRequestURI(), message );
+//        return handleExceptionInternal(errorCode, message);
+//    }
+//
+//    /*
+//     * Request Body 검증 에러
+//     */
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    public ResponseEntity<Object> handleRequestBodyValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
+//        ErrorCode errorCode = ErrorCode.INVALID_REQUEST_BODY;
+//        String message = makeBindingErrorMessage(e.getBindingResult());
+//        log.warn("[{}] Request Body 이상.. {}", request.getRequestURL(), message );
+//        return handleExceptionInternal(errorCode, message);
+//    }
+//    @Override
+//    public ResponseEntity<Object> handleMethodArgumentNotValid(
+//            MethodArgumentNotValidException e,
+//            HttpHeaders headers,
+//            HttpStatus status,
+//            WebRequest request) {
+//        ErrorCode errorCode = ErrorCode.INVALID_REQUEST_BODY;
+//        String message = makeBindingErrorMessage(e.getBindingResult());
+//        log.warn("Request Body 이상.. {}", message );
+//        return handleExceptionInternal(errorCode, message);
+//    }
 
     /*
      * 이외 에러
      */
-    @ExceptionHandler(Exception.class)
-    protected ResponseEntity<Object> handleUnExpectedException(Exception e) {
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<Object> handleUnExpectedException(Exception e) {
         ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
         log.error(e.getMessage());
         return handleExceptionInternal(errorCode);
