40b3dbcb70a0cc585debc24b46f0726f30917a4a
NaayoungKwon <kny8092@gmail.com>
2023-01-19 00:02:59 +0900

    feat/#12 : Vacation Entity 설정

--- src/main/java/io/eagle/wealthmarblebackend/common/BaseEntity.java
+++ src/main/java/io/eagle/wealthmarblebackend/common/BaseEntity.java
@@ -1,6 +1,6 @@
 package io.eagle.wealthmarblebackend.common;
 
-import lombok.Data;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -9,7 +9,7 @@ import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
-@Data
+@Getter
 @MappedSuperclass
 @EntityListeners(value = AuditingEntityListener.class)
 public abstract class BaseEntity {
--- src/main/java/io/eagle/wealthmarblebackend/domain/cahoots/domain/type/ThemeLocationType.java
+++ src/main/java/io/eagle/wealthmarblebackend/domain/cahoots/domain/type/ThemeLocationType.java
@@ -1,7 +1,13 @@
 package io.eagle.wealthmarblebackend.domain.cahoots.domain.type;
 
 public enum ThemeLocationType {
-    BEACH,
-    FOREST,
-    DOWNTOWN
+    BEACH("BEACH"),
+    FOREST("FOREST"),
+    DOWNTOWN("DOWNTOWN");
+
+    private String type;
+
+    ThemeLocationType(String type) {
+        this.type = type;
+    }
 }
