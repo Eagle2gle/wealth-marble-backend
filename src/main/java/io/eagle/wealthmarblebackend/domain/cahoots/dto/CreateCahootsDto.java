1edcdf1de445bd86427855158881f84dee4369bc
NaayoungKwon <kny8092@gmail.com>
2023-01-19 00:03:47 +0900

    feat/#12 : 공모 생성 dto 수정

--- src/main/java/io/eagle/wealthmarblebackend/domain/cahoots/dto/CreateCahootsDto.java
+++ src/main/java/io/eagle/wealthmarblebackend/domain/cahoots/dto/CreateCahootsDto.java
@@ -1,12 +1,15 @@
 package io.eagle.wealthmarblebackend.domain.cahoots.dto;
 
+import io.eagle.wealthmarblebackend.domain.cahoots.domain.type.ThemeBuildingType;
+import io.eagle.wealthmarblebackend.domain.cahoots.domain.type.ThemeLocationType;
 import lombok.*;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -19,20 +22,18 @@ public class CreateCahootsDto {
     @Length(min = 5, max = 15, message = "공모 제목을 5자 이상, 15자 미만으로 작성해주세요.")
     private String title;
 
-    @NotBlank(message = "휴가지 테마를 확인해주세요.")
-    private String themeLocation;
+    private ThemeLocationType themeLocation;
 
-    @NotBlank(message = "휴가지 테마를 확인해주세요.")
-    private String themeBuilding;
+    private ThemeBuildingType themeBuilding;
 
     @NotBlank(message = "휴가지 위치를 확인해주세요.")
     private String location;
 
-    @NotNull(message = "휴가지 건설 예상 시간을 확인해주세요.")
+    @Positive(message = "휴가지 건설 예상 시간을 확인해주세요.")
     private Integer expectedMonth;
 
-    @NotNull(message = "휴가지 건설 진행 예상 시간을 확인해주세요.")
-//    @Max(1000000000000)
+    @Positive(message = "휴가지 건설 진행 예상 비용을 확인해주세요.")
+    @Max(100000000)
     private Long expectedTotalCost;
 
     @NotBlank
@@ -44,16 +45,18 @@ public class CreateCahootsDto {
     private String descritption;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
-//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     private LocalDate stockStart;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
-//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     private LocalDate stockEnd;
-    @NotNull(message = "1주당 공모 가격을 작성해주세요.")
+
+    @Positive(message = "1주당 공모 가격을 작성해주세요.")
     private Long stockPrice;
-    @NotNull(message = "목표 공모 주식 수를 작성해주세요.")
+
+    @Positive(message = "목표 공모 주식 수를 작성해주세요.")
     private Integer stockNum;
+
+    // TODO : hashtag
 
     // TODO : picture url
     private List<MultipartFile> images = new ArrayList<>();
