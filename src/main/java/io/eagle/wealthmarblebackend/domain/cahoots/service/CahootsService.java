76d3f30595441553a001ff25b8b3d9dacf1f973f
NaayoungKwon <kny8092@gmail.com>
2023-01-19 00:45:38 +0900

    feat/#12 : 공모 생성 비지니스로직 에러 검증 추가
     - 현재 날짜보다 뒤이면 에러처리

--- src/main/java/io/eagle/wealthmarblebackend/domain/cahoots/service/CahootsService.java
+++ src/main/java/io/eagle/wealthmarblebackend/domain/cahoots/service/CahootsService.java
@@ -3,8 +3,12 @@ package io.eagle.wealthmarblebackend.domain.cahoots.service;
 import io.eagle.wealthmarblebackend.domain.cahoots.domain.Cahoots;
 import io.eagle.wealthmarblebackend.domain.cahoots.domain.CahootsRepository;
 import io.eagle.wealthmarblebackend.domain.cahoots.dto.CreateCahootsDto;
+import io.eagle.wealthmarblebackend.exception.ApiException;
+import io.eagle.wealthmarblebackend.exception.error.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 @RequiredArgsConstructor
@@ -12,20 +16,24 @@ public class CahootsService {
 
     private final CahootsRepository cahootsRepository;
     public void create(CreateCahootsDto createCahootsDto) {
-        // input 추가 검증
         validateCahootsPeriod(createCahootsDto);
         // TODO : 사진 업로드
-
-        // entity 변환
         // TODO : 요청 사용자의 정보 추가
         Cahoots newCahoots = new Cahoots(createCahootsDto);
-        // 저장
         cahootsRepository.save(newCahoots);
     }
 
+    /*
+    * 공모 시작 및 마감 날짜 검증
+    * */
     private void validateCahootsPeriod(CreateCahootsDto createCahootsDto){
-        if(createCahootsDto.getStockStart().compareTo(createCahootsDto.getStockEnd()) > 0) {
-            // throw exception
+        LocalDate today = LocalDate.now();
+        if(
+                createCahootsDto.getStockStart().compareTo(createCahootsDto.getStockEnd()) > 0
+                || today.compareTo(createCahootsDto.getStockStart()) > 0
+                || today.compareTo(createCahootsDto.getStockEnd()) > 0
+        ) {
+            throw new ApiException(ErrorCode.VACATION_PERIOD_INVALID);
         }
     }
 }
