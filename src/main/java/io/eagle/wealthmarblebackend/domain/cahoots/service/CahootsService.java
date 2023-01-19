fe76f14c11af28ba02677c9e4d48e453f550c54c
NaayoungKwon <kny8092@gmail.com>
2023-01-19 20:45:27 +0900

    refactor : cahoots repo -> vacation repo

--- src/main/java/io/eagle/wealthmarblebackend/domain/cahoots/service/CahootsService.java
+++ src/main/java/io/eagle/wealthmarblebackend/domain/cahoots/service/CahootsService.java
@@ -1,7 +1,7 @@
 package io.eagle.wealthmarblebackend.domain.cahoots.service;
 
-import io.eagle.wealthmarblebackend.domain.cahoots.domain.Cahoots;
-import io.eagle.wealthmarblebackend.domain.cahoots.domain.CahootsRepository;
+import io.eagle.wealthmarblebackend.domain.cahoots.domain.VacationRepository;
+import io.eagle.wealthmarblebackend.domain.cahoots.domain.Vacation;
 import io.eagle.wealthmarblebackend.domain.cahoots.dto.CreateCahootsDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,13 +11,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CahootsService {
 
-    private final CahootsRepository cahootsRepository;
+    private final VacationRepository vacationRepository;
     public void create(CreateCahootsDto createCahootsDto) {
         createCahootsDto.validateCahootsPeriod();
         // TODO : 사진 업로드
         // TODO : 요청 사용자의 정보 추가
-        Cahoots newCahoots = new Cahoots(createCahootsDto);
-        cahootsRepository.save(newCahoots);
+        Vacation newVacation = new Vacation(createCahootsDto);
+        vacationRepository.save(newVacation);
     }
 
 
