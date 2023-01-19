36eaf782658d04666e8b2fd16d5aa2768c92acaf
NaayoungKwon <kny8092@gmail.com>
2023-01-19 23:27:39 +0900

    feat/#13 : sum 사용을 위한 querydsl 설정

--- src/main/java/io/eagle/wealthmarblebackend/domain/cahoots/domain/ContestParticipationRepository.java
+++
@@ -1,8 +0,0 @@
-package io.eagle.wealthmarblebackend.domain.cahoots.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContestParticipationRepository extends JpaRepository<ContestParticipation, Long> {
-}
