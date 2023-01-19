adbb6490f63938391096088cba203eafa481c0ab
NaayoungKwon <kny8092@gmail.com>
2023-01-19 23:32:20 +0900

    feat/#13 : 공모 상세정보 전달 service layer

--- src/main/java/io/eagle/wealthmarblebackend/domain/cahoots/service/CahootsService.java
+++ src/main/java/io/eagle/wealthmarblebackend/domain/cahoots/service/CahootsService.java
@@ -1,8 +1,12 @@
 package io.eagle.wealthmarblebackend.domain.cahoots.service;
 
+import io.eagle.wealthmarblebackend.domain.cahoots.domain.ContestParticipation.ContestParticipationRepository;
 import io.eagle.wealthmarblebackend.domain.cahoots.domain.VacationRepository;
 import io.eagle.wealthmarblebackend.domain.cahoots.domain.Vacation;
 import io.eagle.wealthmarblebackend.domain.cahoots.dto.CreateCahootsDto;
+import io.eagle.wealthmarblebackend.domain.cahoots.dto.DetailCahootsDto;
+import io.eagle.wealthmarblebackend.exception.ApiException;
+import io.eagle.wealthmarblebackend.exception.error.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,12 +16,21 @@ import org.springframework.stereotype.Service;
 public class CahootsService {
 
     private final VacationRepository vacationRepository;
+    private final ContestParticipationRepository contestParticipationRepository;
+
     public void create(CreateCahootsDto createCahootsDto) {
         createCahootsDto.validateCahootsPeriod();
         // TODO : 사진 업로드
         // TODO : 요청 사용자의 정보 추가
         Vacation newVacation = new Vacation(createCahootsDto);
         vacationRepository.save(newVacation);
+    }
+
+    public DetailCahootsDto getDetail(Long cahootsId) {
+        Vacation vacation = vacationRepository.findById(cahootsId).orElseThrow(()-> new ApiException(ErrorCode.VACATION_NOT_FOUND));
+        Integer currentTotalStock = contestParticipationRepository.getCurrentContestNum(cahootsId).get(0);
+        Integer competitionRate = currentTotalStock * 100 / vacation.getStock().getNum();
+        return DetailCahootsDto.toDto(vacation, competitionRate);
     }
 
 
