eaa4130cc02a1b99a93f815d284862dae0841b6d
NaayoungKwon <kny8092@gmail.com>
2023-01-20 23:20:57 +0900

    feat/#19 : 공모 현황 검색 service layer 작성

--- src/main/java/io/eagle/wealthmarblebackend/domain/ContestParticipation/controller/ContestParticipationController.java
+++ src/main/java/io/eagle/wealthmarblebackend/domain/ContestParticipation/controller/ContestParticipationController.java
@@ -1,12 +1,14 @@
 package io.eagle.wealthmarblebackend.domain.ContestParticipation.controller;
 
 
+import io.eagle.wealthmarblebackend.domain.ContestParticipation.dto.HistoryCahootsDto;
+import io.eagle.wealthmarblebackend.domain.ContestParticipation.dto.HistoryCahootsDtoList;
 import io.eagle.wealthmarblebackend.domain.ContestParticipation.service.ContestParticipationService;
-import io.eagle.wealthmarblebackend.domain.vacation.dto.HistoryCahootsDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -22,7 +24,8 @@ public class ContestParticipationController {
     }
 
     @GetMapping(value = "/{cahootsId}", params = "info=history")
-    public HistoryCahootsDto getCahootsHistoryInfo(@PathVariable("cahootsId") Long cahootsId){
-        return contestParticipationService.getHistory(cahootsId);
+    public HistoryCahootsDtoList getCahootsHistoryInfo(@PathVariable("cahootsId") Long cahootsId){
+        List<HistoryCahootsDto> result = contestParticipationService.getHistory(cahootsId);
+        return new HistoryCahootsDtoList(result);
     }
 }
