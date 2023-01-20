1ccf05e6130051c86d4dcd73b2ade5c625b27981
NaayoungKwon <kny8092@gmail.com>
2023-01-20 23:19:47 +0900

    feat/#19 : 공모 현황 dto 작성

--- src/main/java/io/eagle/wealthmarblebackend/domain/ContestParticipation/dto/HistoryCahootsDto.java
+++ src/main/java/io/eagle/wealthmarblebackend/domain/ContestParticipation/dto/HistoryCahootsDto.java
@@ -1,4 +1,23 @@
-package io.eagle.wealthmarblebackend.domain.vacation.dto;
+package io.eagle.wealthmarblebackend.domain.ContestParticipation.dto;
 
+import io.eagle.wealthmarblebackend.domain.ContestParticipation.entity.ContestParticipation;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Data
+@RequiredArgsConstructor
 public class HistoryCahootsDto {
+
+    private final LocalDateTime time;
+
+    private final Integer stocks;
+
+    public static HistoryCahootsDto toDto(ContestParticipation c){
+        return new HistoryCahootsDto(c.getCreatedAt(), c.getStocks());
+    }
 }
