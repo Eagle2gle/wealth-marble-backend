6b78bc73e21a102d52805fb41c420341714614ca
NaayoungKwon <kny8092@gmail.com>
2023-01-20 23:20:41 +0900

    feat/#19 : 공모 현황 검색 repository 작성

--- src/main/java/io/eagle/wealthmarblebackend/domain/ContestParticipation/repository/ContestParticipationRepositoryCustom.java
+++ src/main/java/io/eagle/wealthmarblebackend/domain/ContestParticipation/repository/ContestParticipationRepositoryCustom.java
@@ -1,9 +1,10 @@
 package io.eagle.wealthmarblebackend.domain.ContestParticipation.repository;
 
+import io.eagle.wealthmarblebackend.domain.ContestParticipation.entity.ContestParticipation;
 import java.util.List;
 import java.util.Optional;
 
 public interface ContestParticipationRepositoryCustom {
     Optional<Integer> getCurrentContestNum(Long cahootsId);
-
+    List<ContestParticipation> findAllByCahootsId(Long cahootsId);
 }
