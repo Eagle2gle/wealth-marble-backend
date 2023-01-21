a299cf13700ebcfc6d668d2b9f7afb307ced0b99
NaayoungKwon <kny8092@gmail.com>
2023-01-22 01:28:09 +0900

    feat/#26 : 사진 url 저장용 db entity

---
+++ src/main/java/io/eagle/wealthmarblebackend/domain/picture/repository/PictureRepository.java
@@ -0,0 +1,7 @@
+package io.eagle.wealthmarblebackend.domain.picture.repository;
+
+import io.eagle.wealthmarblebackend.domain.picture.entity.Picture;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PictureRepository extends JpaRepository<Picture, Long> {
+}
