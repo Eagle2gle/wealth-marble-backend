b87508226dc03f9a58a3816f683f1156a993d21d
NaayoungKwon <kny8092@gmail.com>
2023-01-24 03:04:26 +0900

    feat/#23 : 진행 중인 공모 간략 정보 service layer 작성

---
+++ src/main/java/io/eagle/wealthmarblebackend/domain/vacation/dto/BreifCahootsDto.java
@@ -0,0 +1,31 @@
+package io.eagle.wealthmarblebackend.domain.vacation.dto;
+
+import io.eagle.wealthmarblebackend.domain.vacation.entity.type.VacationStatusType;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+public class BreifCahootsDto {
+    private Long id;
+    private String title;
+    private String location;
+    private VacationStatusType status;
+
+    private LocalDate stockStart;
+
+    private LocalDate stockEnd;
+
+    private Long stockPrice;
+
+    private Integer competitionRate;
+
+    // TODO : hashtag
+
+    // TODO : picture url
+//    private final List<String> images = new ArrayList<>();
+
+}
