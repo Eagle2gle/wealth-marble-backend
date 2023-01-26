8fb81f3df1b47e569ece5b104cb5a89c4f7161e3
NaayoungKwon <kny8092@gmail.com>
2023-01-26 13:24:56 +0900

    refactor: Qentity static 변경

--- src/main/java/io/eagle/wealthmarblebackend/domain/ContestParticipation/repository/ContestParticipationRepositoryImpl.java
+++ src/main/java/io/eagle/wealthmarblebackend/domain/ContestParticipation/repository/ContestParticipationRepositoryImpl.java
@@ -2,11 +2,12 @@ package io.eagle.wealthmarblebackend.domain.ContestParticipation.repository;
 
 import com.querydsl.jpa.JPQLQueryFactory;
 import io.eagle.wealthmarblebackend.domain.ContestParticipation.entity.ContestParticipation;
-import io.eagle.wealthmarblebackend.domain.ContestParticipation.entity.QContestParticipation;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 import java.util.Optional;
+
+import static io.eagle.wealthmarblebackend.domain.ContestParticipation.entity.QContestParticipation.contestParticipation;
 
 @RequiredArgsConstructor
 public class ContestParticipationRepositoryImpl implements ContestParticipationRepositoryCustom {
@@ -14,21 +15,19 @@ public class ContestParticipationRepositoryImpl implements ContestParticipationR
 
     @Override
     public Optional<Integer> getCurrentContestNum(Long cahootsId) {
-        QContestParticipation cp = QContestParticipation.contestParticipation;
         return Optional.ofNullable(queryFactory
-                    .select(cp.stocks.sum())
-                    .from(cp)
-                    .where(cp.vacation.id.eq(cahootsId))
+                    .select(contestParticipation.stocks.sum())
+                    .from(contestParticipation)
+                    .where(contestParticipation.vacation.id.eq(cahootsId))
                     .fetchOne());
     }
 
     @Override
     public List<ContestParticipation> findAllByCahootsId(Long cahootsId){
-        QContestParticipation cp = QContestParticipation.contestParticipation;
         return queryFactory
-                .selectFrom(cp)
-                .where(cp.vacation.id.eq(cahootsId))
-                .orderBy(cp.createdAt.desc())
+                .selectFrom(contestParticipation)
+                .where(contestParticipation.vacation.id.eq(cahootsId))
+                .orderBy(contestParticipation.createdAt.desc())
                 .fetch();
 
     }
