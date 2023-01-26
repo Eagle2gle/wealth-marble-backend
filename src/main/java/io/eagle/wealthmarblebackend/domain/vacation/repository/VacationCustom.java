dcec65d77aaec5416006651367b932e02201003b
NaayoungKwon <kny8092@gmail.com>
2023-01-26 14:39:37 +0900

    feat/38: 최신 공모 repository 작성

--- src/main/java/io/eagle/wealthmarblebackend/domain/vacation/repository/VacationCustom.java
+++ src/main/java/io/eagle/wealthmarblebackend/domain/vacation/repository/VacationCustom.java
@@ -1,9 +1,6 @@
 package io.eagle.wealthmarblebackend.domain.vacation.repository;
 
-import io.eagle.wealthmarblebackend.domain.vacation.dto.BreifCahootsDto;
-import io.eagle.wealthmarblebackend.domain.vacation.dto.DetailCahootsDto;
-import io.eagle.wealthmarblebackend.domain.vacation.dto.BreifV2CahootsDto;
-import io.eagle.wealthmarblebackend.domain.vacation.dto.InfoConditionDto;
+import io.eagle.wealthmarblebackend.domain.vacation.dto.*;
 
 import java.util.List;
 
@@ -11,4 +8,5 @@ public interface VacationCustom {
     DetailCahootsDto getVacationDetail(Long cahootsId);
     List<BreifCahootsDto> getVacationsBreif(InfoConditionDto infoConditionDto);
     List<BreifV2CahootsDto> getVacationsBreifV2(InfoConditionDto infoConditionDto);
+    List<LatestCahootsDto> findLatestVacations();
 }
--- src/main/java/io/eagle/wealthmarblebackend/domain/vacation/repository/VacationCustomImpl.java
+++ src/main/java/io/eagle/wealthmarblebackend/domain/vacation/repository/VacationCustomImpl.java
@@ -5,15 +5,13 @@ import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPQLQueryFactory;
-import io.eagle.wealthmarblebackend.domain.vacation.dto.BreifCahootsDto;
-import io.eagle.wealthmarblebackend.domain.vacation.dto.BreifV2CahootsDto;
-import io.eagle.wealthmarblebackend.domain.vacation.dto.InfoConditionDto;
-import io.eagle.wealthmarblebackend.domain.vacation.dto.DetailCahootsDto;
+import io.eagle.wealthmarblebackend.domain.vacation.dto.*;
 import io.eagle.wealthmarblebackend.domain.vacation.entity.type.VacationStatusType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import static io.eagle.wealthmarblebackend.domain.ContestParticipation.entity.QContestParticipation.*;
@@ -30,6 +28,9 @@ public class VacationCustomImpl implements VacationCustom {
 
     @Value("${eagle.int.before_day}")
     private Integer beforeDays;
+
+    @Value("${eagle.int.recent_day}")
+    private Integer recentDays;
 
 
     public DetailCahootsDto getVacationDetail(Long cahootsId){
@@ -91,10 +92,24 @@ public class VacationCustomImpl implements VacationCustom {
                         vacation.stockPeriod.start.as("stockStart"),
                         vacation.stockPeriod.end.as("stockEnd")))
                 .from(vacation)
-                .where(isInStatus(infoConditionDto.getTypes()), isInDateRange())
+                .where(isInStatus(infoConditionDto.getTypes()), isInDateRange(beforeDays))
                 .orderBy(vacation.stockPeriod.end.asc())
                 .limit(this.page)
                 .offset(infoConditionDto.getOffset() * this.page)
+                .fetch();
+    }
+
+    public List<LatestCahootsDto> findLatestVacations(){
+        return queryFactory
+                .select(Projections.fields(LatestCahootsDto.class,
+                        vacation.id,
+                        vacation.title,
+                        vacation.stockPeriod.start.as("stockStart"),
+                        vacation.expectedRateOfReturn))
+                .from(vacation)
+                .where(isInStatus(new VacationStatusType[]{VacationStatusType.CAHOOTS_ONGOING}), isInDateRange(recentDays))
+                .orderBy(vacation.stockPeriod.start.desc())
+                .limit(this.page)
                 .fetch();
     }
 
@@ -107,8 +122,8 @@ public class VacationCustomImpl implements VacationCustom {
         return keyword != null && !keyword.isBlank() ? (vacation.title.contains(keyword).or(vacation.location.contains(keyword))) : null;
     }
 
-    private BooleanExpression isInDateRange(){
+    private BooleanExpression isInDateRange(Integer interval){
         LocalDate today = LocalDate.now();
-        return vacation.stockPeriod.end.between(today, today.plusDays(beforeDays));
+        return vacation.stockPeriod.end.between(today, today.plusDays(interval));
     }
 }
