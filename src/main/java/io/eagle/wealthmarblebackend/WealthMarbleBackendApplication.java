4f95f38c8992792a936d9b53cddca39123a78aef
CEOJINSUNG <jinsung1048@gmail.com>
2023-01-27 12:43:04 +0900

    feat/#41 : Security 활성화

--- src/main/java/io/eagle/wealthmarblebackend/WealthMarbleBackendApplication.java
+++ src/main/java/io/eagle/wealthmarblebackend/WealthMarbleBackendApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@EnableWebSecurity
 @EnableJpaAuditing
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class WealthMarbleBackendApplication {
 
 	public static void main(String[] args) {
