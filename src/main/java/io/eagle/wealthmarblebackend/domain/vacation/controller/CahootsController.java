d7f1fbe8214e90cab47de37464f176a5b10d556d
NaayoungKwon <kny8092@gmail.com>
2023-01-31 10:56:17 +0900

    feat/#50: 응답 형식 표준화

--- src/main/java/io/eagle/wealthmarblebackend/domain/vacation/controller/CahootsController.java
+++ src/main/java/io/eagle/wealthmarblebackend/domain/vacation/controller/CahootsController.java
@@ -1,5 +1,6 @@
 package io.eagle.wealthmarblebackend.domain.vacation.controller;
 
+import io.eagle.wealthmarblebackend.common.ApiResponse;
 import io.eagle.wealthmarblebackend.domain.vacation.dto.*;
 import io.eagle.wealthmarblebackend.domain.vacation.entity.type.VacationStatusType;
 import io.eagle.wealthmarblebackend.domain.vacation.service.CahootsService;
@@ -16,41 +17,41 @@ public class CahootsController {
     private final CahootsService cahootsService;
 
     @PostMapping
-    public ResponseEntity createCahoots (@Valid CreateCahootsDto createCahootsDto){
+    public ApiResponse createCahoots (@Valid CreateCahootsDto createCahootsDto){
         cahootsService.create(createCahootsDto);
-        return ResponseEntity.ok().build();
+        return ApiResponse.createSuccessWithNoContent();
     }
 
     @GetMapping(value = "/{cahootsId}", params = "info=detail")
-    public DetailCahootsDto getCahootsDetailInfo(@PathVariable("cahootsId") Long cahootsId){
-        return cahootsService.getDetail(cahootsId);
+    public ApiResponse getCahootsDetailInfo(@PathVariable("cahootsId") Long cahootsId){
+        return ApiResponse.createSuccess(cahootsService.getDetail(cahootsId));
     }
 
     @GetMapping(params = "status=ongoing")
-    public BreifCahootsListDto getBreifCahootsInfo(@Valid InfoConditionDto infoConditionDto){
+    public ApiResponse getBreifCahootsInfo(@Valid InfoConditionDto infoConditionDto){
         System.out.println(infoConditionDto);
         infoConditionDto.setTypes(new VacationStatusType[]{VacationStatusType.CAHOOTS_ONGOING});
-        return cahootsService.getBreifList(infoConditionDto);
+        return ApiResponse.createSuccess(cahootsService.getBreifList(infoConditionDto));
     }
 
 
 
     @GetMapping(params = "status=ended")
-    public BreifCahootsListDto getEndedBreifCahootsInfo(){
+    public ApiResponse getEndedBreifCahootsInfo(){
         VacationStatusType[] types = new VacationStatusType[]{VacationStatusType.CAHOOTS_CLOSE, VacationStatusType.CAHOOTS_OPEN};
         InfoConditionDto infoConditionDto = InfoConditionDto.builder().types(types).offset(0).build();
-        return cahootsService.getBreifList(infoConditionDto);
+        return ApiResponse.createSuccess(cahootsService.getBreifList(infoConditionDto));
     }
 
     @GetMapping(params = "status=ending-soon")
-    public BreifV2CahootsListDto getEndedSoonBreifCahootsInfo(){
+    public ApiResponse getEndedSoonBreifCahootsInfo(){
         VacationStatusType[] types = new VacationStatusType[]{VacationStatusType.CAHOOTS_ONGOING};
         InfoConditionDto infoConditionDto = InfoConditionDto.builder().types(types).offset(0).build();
-        return cahootsService.getBreifV2List(infoConditionDto);
+        return ApiResponse.createSuccess(cahootsService.getBreifV2List(infoConditionDto));
     }
 
     @GetMapping("/recent")
-    public LatestCahootsListDto getlatestCahootsInfo(){
-        return cahootsService.getLatestsList();
+    public ApiResponse getlatestCahootsInfo(){
+        return ApiResponse.createSuccess(cahootsService.getLatestsList());
     }
 }
