066092f337f90dc9f1d1a84dd2019f19b0d122ce
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-07 15:16:22 +0900

    feat/#48 : findAllByUser leftjoin으로 구현

--- api/src/main/java/io/eagle/domain/interest/repository/InterestRepositoryImpl.java
+++ api/src/main/java/io/eagle/domain/interest/repository/InterestRepositoryImpl.java
@@ -1,13 +1,29 @@
 package io.eagle.domain.interest.repository;
 
 import com.querydsl.jpa.JPQLQueryFactory;
+import io.eagle.entity.Interest;
+import io.eagle.entity.User;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static io.eagle.entity.QInterest.*;
 
 @RequiredArgsConstructor
 public class InterestRepositoryImpl implements InterestRepositoryCustom {
 
     private final JPQLQueryFactory jpqlQueryFactory;
 
+    @Override
+    public List<Interest> findAllByUser(User user) {
+        return jpqlQueryFactory
+            .selectFrom(interest)
+            .leftJoin(interest.user)
+            .where(interest.user.id.eq(user.getId()))
+            .fetchAll()
+            .stream()
+            .collect(Collectors.toList());
+    }
 
 }
