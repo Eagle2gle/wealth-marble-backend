5c724ef4ada7efc7559c424dd5f71965d92a49d9
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-07 22:34:18 +0900

    feat/#71 : MarketDetailDto 생성

---
+++ api/src/main/java/io/eagle/domain/vacation/dto/MarketDetailDto.java
@@ -0,0 +1,27 @@
+package io.eagle.domain.vacation.dto;
+
+import io.eagle.entity.Picture;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Getter
+@Builder
+public class MarketDetailDto {
+
+    @NotNull
+    private String title;
+    @NotNull
+    private String location;
+    @NotNull
+    private String shortDescription;
+    @NotNull
+    private Integer expectedRateOfReturn;
+    @NotNull
+    private Integer price;
+    private String picture;
+    private List<Long> userIds;
+
+}
