84a6be9a728fca1bccfcef11c6e189e5ead6a123
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-07 19:40:08 +0900

    feat/#48 : InterestInfoDto 생성

---
+++ api/src/main/java/io/eagle/domain/interest/dto/InterestInfoDto.java
@@ -0,0 +1,22 @@
+package io.eagle.domain.interest.dto;
+
+import io.eagle.entity.Picture;
+import io.eagle.entity.Vacation;
+import lombok.Getter;
+
+@Getter
+public class InterestInfoDto {
+
+    private String title;
+    private String shortDescription;
+    private String location;
+    private Picture picture;
+
+    public InterestInfoDto(Vacation vacation) {
+        this.title = vacation.getTitle();
+        this.shortDescription = vacation.getShortDescription();
+        this.location = vacation.getLocation();
+        this.picture = vacation.getPictureList().get(0);
+    }
+
+}
