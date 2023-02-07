7b02413cca244dd062a5d81915b882c332c5a676
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-07 21:04:00 +0900

    feat/#68 : [POST] /api/interests 제작

--- api/src/main/java/io/eagle/domain/interest/controller/InterestController.java
+++ api/src/main/java/io/eagle/domain/interest/controller/InterestController.java
@@ -1,11 +1,15 @@
 package io.eagle.domain.interest.controller;
 
 import io.eagle.common.ApiResponse;
+import io.eagle.domain.interest.dto.InterestDto;
 import io.eagle.domain.interest.service.InterestService;
+import io.eagle.entity.Interest;
 import io.eagle.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController(value = "/api/interests")
@@ -17,5 +21,11 @@ public class InterestController {
     @GetMapping("/me/interest")
     public ApiResponse getAllUserInterests(@AuthenticationPrincipal User user) {
         return ApiResponse.createSuccess(interestService.getAllInterest(user));
+    }
+
+    @PostMapping("/")
+    public ApiResponse createInterest(@RequestBody InterestDto interestDto) {
+        Interest interest = interestService.createInterest(interestDto);
+        return interest != null ? ApiResponse.createSuccess(interest) : ApiResponse.createError("해당 유저나 휴가 정보가 존재하지 않습니다.");
     }
 }
