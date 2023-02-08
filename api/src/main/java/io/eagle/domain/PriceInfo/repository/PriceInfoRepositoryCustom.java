b02be93a138d22b0e0fcfbfde2939454231d86f2
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-08 18:46:25 +0900

    feat/#75 : findAllByDateOrderByCreatedAt 생성

--- api/src/main/java/io/eagle/domain/PriceInfo/repository/PriceInfoRepositoryCustom.java
+++ api/src/main/java/io/eagle/domain/PriceInfo/repository/PriceInfoRepositoryCustom.java
@@ -1,4 +1,10 @@
 package io.eagle.domain.PriceInfo.repository;
 
+import io.eagle.domain.PriceInfo.dto.ChartRequestDto;
+import io.eagle.domain.PriceInfo.dto.ChartResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface PriceInfoRepositoryCustom {
+    Page<ChartResponseDto> findAllByDateOrderByCreatedAt(Pageable pageable, ChartRequestDto chartRequestDto);
 }
