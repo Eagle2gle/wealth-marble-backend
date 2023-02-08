4067cca239f0d87702380d07d0b9e1a5d88d5b54
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-08 22:33:00 +0900

    feat/#75 : 데이터 가져오는 로직 생성

--- api/src/main/java/io/eagle/domain/PriceInfo/service/PriceInfoService.java
+++ api/src/main/java/io/eagle/domain/PriceInfo/service/PriceInfoService.java
@@ -1,8 +1,12 @@
 package io.eagle.domain.PriceInfo.service;
 
+import io.eagle.domain.PriceInfo.dto.ChartRequestDto;
+import io.eagle.domain.PriceInfo.dto.ChartResponseDto;
 import io.eagle.domain.PriceInfo.repository.PriceInfoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +14,7 @@ public class PriceInfoService {
 
     private final PriceInfoRepository priceInfoRepository;
 
+    public List<ChartResponseDto> getVacationChartData(Long vacationId, ChartRequestDto chartRequestDto) {
+        return priceInfoRepository.findAllByVacationOrderByCreatedAt(vacationId, chartRequestDto);
+    }
 }
