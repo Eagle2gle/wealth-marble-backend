bc7d0043e40d2153411f3f7bb2d37118bbf4de57
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-08 22:16:35 +0900

    refactor/#75 : @RequestMapping 추가

--- api/src/main/java/io/eagle/domain/vacation/controller/MarketController.java
+++ api/src/main/java/io/eagle/domain/vacation/controller/MarketController.java
@@ -5,15 +5,17 @@ import io.eagle.domain.vacation.service.MarketService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/markets")
+@RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class MarketController {
 
     private final MarketService marketService;
 
-    @GetMapping("/{vacationId}")
+    @GetMapping("/markets/{vacationId}")
     public ApiResponse getOne(@PathVariable("vacationId") Long vacationId) {
         return ApiResponse.createSuccess(marketService.getOne(vacationId));
     }
