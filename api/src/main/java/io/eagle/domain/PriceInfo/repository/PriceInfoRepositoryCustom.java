67aba65d994f4a8776a5b463e531db1490f90ceb
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-08 18:57:01 +0900

    refactor/#75 : page 에서 list 로 변경

--- api/src/main/java/io/eagle/domain/PriceInfo/repository/PriceInfoRepositoryCustom.java
+++ api/src/main/java/io/eagle/domain/PriceInfo/repository/PriceInfoRepositoryCustom.java
@@ -2,9 +2,9 @@ package io.eagle.domain.PriceInfo.repository;
 
 import io.eagle.domain.PriceInfo.dto.ChartRequestDto;
 import io.eagle.domain.PriceInfo.dto.ChartResponseDto;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface PriceInfoRepositoryCustom {
-    Page<ChartResponseDto> findAllByDateOrderByCreatedAt(Pageable pageable, ChartRequestDto chartRequestDto);
+    List<ChartResponseDto> findAllByDateOrderByCreatedAt(ChartRequestDto chartRequestDto);
 }
