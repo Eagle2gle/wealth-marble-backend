20c0a6ce5e25c0678a181f59bdbfaa3c886f4fe6
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-08 18:41:20 +0900

    feat/#75 : ChartResponseDto로 이름 변경

--- api/src/main/java/io/eagle/domain/PriceInfo/dto/ChartResponseDto.java
+++ api/src/main/java/io/eagle/domain/PriceInfo/dto/ChartResponseDto.java
@@ -6,14 +6,14 @@ import lombok.Getter;
 import java.util.Date;
 
 @Getter
-public class ChartDto {
+public class ChartResponseDto {
 
     private Date date;
     private Integer price;
     private Integer transactionAmount;
 
     @QueryProjection
-    public ChartDto(Date date, Integer price, Integer transactionAmount) {
+    public ChartResponseDto(Date date, Integer price, Integer transactionAmount) {
         this.date = date;
         this.price = price;
         this.transactionAmount = transactionAmount;
