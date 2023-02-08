2caf8e2745c54107ffb59884ebd01631b68b17f9
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-08 22:16:31 +0900

    refactor/#75 : @RequestMapping 추가

--- api/src/main/java/io/eagle/domain/transaction/controller/TransactionController.java
+++ api/src/main/java/io/eagle/domain/transaction/controller/TransactionController.java
@@ -4,15 +4,17 @@ import io.eagle.domain.transaction.service.TransactionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/transactions")
+@RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class TransactionController {
 
     private final TransactionService transactionService;
 
-    @GetMapping("/me")
+    @GetMapping("/transactions/me")
     public ResponseEntity getTransactionHistoryByMine() {
         Long userId = 1L;
         return ResponseEntity.ok(transactionService.getMineTransaction(userId));
