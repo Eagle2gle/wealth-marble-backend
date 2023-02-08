7ce69d4b32e1cda64624a09e9fe1be1423fcf62d
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-08 19:12:09 +0900

    feat/#75 : @QueryStringArgResolver 작업

---
+++ api/src/main/java/io/eagle/common/QueryStringArgResolver.java
@@ -0,0 +1,11 @@
+package io.eagle.common;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface QueryStringArgResolver {
+}
