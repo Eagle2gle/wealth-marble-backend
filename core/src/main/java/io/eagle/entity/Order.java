35c85a7db589bd428d2e05ba4748a5b3ea2c1fed
NaayoungKwon <kny8092@gmail.com>
2023-02-10 02:52:46 +0900

    feat/#70 : order entity dynamic update, 외래키 참조 추가

--- core/src/main/java/io/eagle/entity/Order.java
+++ core/src/main/java/io/eagle/entity/Order.java
@@ -2,13 +2,18 @@ package io.eagle.entity;
 
 import io.eagle.entity.type.OrderStatus;
 import io.eagle.entity.type.OrderType;
-import lombok.Data;
+import lombok.*;
+import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
 
 @Data
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name="orders")
+@DynamicUpdate
 public class Order extends BaseEntity {
 
     @Id
@@ -20,8 +25,11 @@ public class Order extends BaseEntity {
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "vacation_id")
+    @JoinColumn(name = "vacation_id", insertable = false, updatable = false) // id만으로 repository.save 가능하도록 옵션 설정
     private Vacation vacation;
+
+    @Column(name="vacation_id") // id만으로 repository.save 가능
+    private Long vacationId;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
@@ -37,4 +45,14 @@ public class Order extends BaseEntity {
     @Column(nullable = false)
     private OrderStatus status;
 
+    public Order deepCopy(){
+        return Order.builder()
+                .status(getStatus())
+                .vacationId(getVacationId())
+                .price(getPrice())
+                .user(getUser())
+                .orderType(getOrderType())
+                .amount(getAmount())
+                .build();
+    }
 }
