9e499d33b392c5e526cdc200570c601da5b35478
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-10 21:23:42 +0900

    feat/#75 : getVacationChartData 데이터 수정

--- api/src/main/java/io/eagle/domain/PriceInfo/service/PriceInfoService.java
+++ api/src/main/java/io/eagle/domain/PriceInfo/service/PriceInfoService.java
@@ -2,7 +2,9 @@ package io.eagle.domain.PriceInfo.service;
 
 import io.eagle.domain.PriceInfo.dto.ChartRequestDto;
 import io.eagle.domain.PriceInfo.dto.ChartResponseDto;
+import io.eagle.domain.PriceInfo.dto.ChartTotalDto;
 import io.eagle.domain.PriceInfo.repository.PriceInfoRepository;
+import io.eagle.entity.PriceInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +16,17 @@ public class PriceInfoService {
 
     private final PriceInfoRepository priceInfoRepository;
 
-    public List<ChartResponseDto> getVacationChartData(Long vacationId, ChartRequestDto chartRequestDto) {
-        return priceInfoRepository.findAllByVacationOrderByCreatedAt(vacationId, chartRequestDto);
+    public ChartTotalDto getVacationChartData(Long vacationId, ChartRequestDto chartRequestDto) {
+        List<ChartResponseDto> chartResponseDtos = priceInfoRepository.findAllByVacationOrderByCreatedAt(vacationId, chartRequestDto);
+        PriceInfo yesterdayInfo = priceInfoRepository.getYesterdayPriceInfo();
+        return ChartTotalDto
+            .builder()
+            .standardDate(yesterdayInfo.getCreatedAt().toLocalDate())
+            .standardPrice(yesterdayInfo.getStandardPrice())
+            .highPrice(yesterdayInfo.getHighPrice())
+            .lowPrice(yesterdayInfo.getLowPrice())
+            .transactionAmount(yesterdayInfo.getTransactionAmount())
+            .chartResponseDtos(chartResponseDtos)
+            .build();
     }
 }
