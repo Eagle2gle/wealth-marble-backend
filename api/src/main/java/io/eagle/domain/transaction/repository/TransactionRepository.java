8e3c34eb0623da3a03a8591c7ef8e86b6c5cb40d
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-11 13:15:05 +0900

    feat/#80 : findByVacationOrderByCreatedAtDesc 제작

--- api/src/main/java/io/eagle/domain/transaction/repository/TransactionRepository.java
+++ api/src/main/java/io/eagle/domain/transaction/repository/TransactionRepository.java
@@ -1,6 +1,7 @@
 package io.eagle.domain.transaction.repository;
 
 import io.eagle.entity.Transaction;
+import io.eagle.entity.Vacation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
