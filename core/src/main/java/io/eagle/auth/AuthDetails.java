ff6e51109eaeb3a2170e69ef131a02a02609431b
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-11 20:55:35 +0900

    feat/#86 : AuthDetail 생성

---
+++ core/src/main/java/io/eagle/auth/AuthDetails.java
@@ -0,0 +1,61 @@
+package io.eagle.auth;
+
+import io.eagle.entity.User;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Data
+public class AuthDetails implements UserDetails {
+
+    private User user;
+
+    public AuthDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collect = new ArrayList<>();
+        collect.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return user.getRole().toString();
+            }
+        });
+        return collect;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getProviderId();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getProviderId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
