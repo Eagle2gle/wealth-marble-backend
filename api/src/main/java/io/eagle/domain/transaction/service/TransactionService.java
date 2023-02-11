f7cce5d74ddf551769f1966c9c4b9dd65c5b0470
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-11 13:15:53 +0900

    feat/#80 : getTransactions 서비스 정의

--- api/src/main/java/io/eagle/domain/transaction/service/TransactionService.java
+++ api/src/main/java/io/eagle/domain/transaction/service/TransactionService.java
@@ -1,16 +1,19 @@
 package io.eagle.domain.transaction.service;
 
 import io.eagle.domain.order.repository.OrderRepository;
+import io.eagle.domain.transaction.dto.TransactionRequestDto;
+import io.eagle.domain.transaction.dto.TransactionResponseDto;
 import io.eagle.domain.transaction.dto.UserTransactionInfoDto;
 import io.eagle.domain.transaction.repository.TransactionRepository;
 import io.eagle.entity.User;
 import io.eagle.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +23,11 @@ public class TransactionService {
     private final UserRepository userRepository;
     private final TransactionRepository transactionRepository;
     private final OrderRepository orderRepository;
+
+    public List<TransactionResponseDto> getTransactions(TransactionRequestDto requestDto) {
+        PageRequest pageRequest = PageRequest.of(requestDto.getPage(), 20, Sort.by(Sort.Direction.DESC, "createdAt"));
+        return transactionRepository.findByVacationOrderByCreatedAtDesc(pageRequest, requestDto);
+    }
 
     public List<UserTransactionInfoDto> getMineTransaction(Long userId) {
         User user = userRepository.findUserById(userId).orElse(null);
