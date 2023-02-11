4246aa42e78161e038ace08394961f5822f4e50e
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-11 13:15:36 +0900

    feat/#80 : findByVacationOrderByCreatedAtDesc 구현

--- api/src/main/java/io/eagle/domain/transaction/repository/TransactionRepositoryImpl.java
+++ api/src/main/java/io/eagle/domain/transaction/repository/TransactionRepositoryImpl.java
@@ -1,10 +1,18 @@
 package io.eagle.domain.transaction.repository;
 
+import com.querydsl.core.types.dsl.DateTemplate;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPQLQueryFactory;
+import io.eagle.domain.transaction.dto.QTransactionResponseDto;
+import io.eagle.domain.transaction.dto.TransactionRequestDto;
+import io.eagle.domain.transaction.dto.TransactionResponseDto;
 import io.eagle.entity.Order;
 import io.eagle.entity.Transaction;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,11 +37,34 @@ public class TransactionRepositoryImpl implements TransactionRepositoryCustom{
 
     @Override
     public Transaction findByVacation(Long vacationId) {
+        System.out.println(vacationId);
         return jpqlQueryFactory
             .selectFrom(transaction)
             .where(transaction.vacation.id.eq(vacationId))
             .orderBy(transaction.createdAt.desc())
-            .fetchOne();
+            .fetchFirst();
+    }
+
+    @Override
+    public List<TransactionResponseDto> findByVacationOrderByCreatedAtDesc(Pageable page, TransactionRequestDto requestDto) {
+        DateTemplate<LocalDateTime> formattedDate = Expressions.dateTemplate(LocalDateTime.class, "DATE_FORMAT({0}, {1})", transaction.createdAt, "%Y-%m-%d");
+        DateTemplate<LocalDateTime> beforeDate = convertIntoDateTemplate(requestDto.getStartDate());
+        DateTemplate<LocalDateTime> afterDate = convertIntoDateTemplate(requestDto.getEndDate());
+        return jpqlQueryFactory
+            .select(new QTransactionResponseDto(transaction.createdAt, transaction.price, transaction.amount))
+            .from(transaction)
+            .where(
+                transaction.vacation.id.eq(requestDto.getVacationId())
+                    .and(formattedDate.between(beforeDate, afterDate))
+            )
+            .offset(page.getOffset())
+            .limit(page.getPageSize())
+            .orderBy(transaction.createdAt.desc())
+            .fetch();
+    }
+
+    private DateTemplate<LocalDateTime> convertIntoDateTemplate(LocalDate date) {
+        return Expressions.dateTemplate(LocalDateTime.class, "DATE_FORMAT({0}, {1})", date, "%Y-%m-%d");
     }
 
 }
