b500a6768f1e633f2fa43d2d64e521addfd061c5
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-13 17:48:21 +0900

    feat/#94 : MARKET_ONGOING 추가

--- core/src/main/java/io/eagle/entity/type/VacationStatusType.java
+++ core/src/main/java/io/eagle/entity/type/VacationStatusType.java
@@ -4,7 +4,8 @@ public enum VacationStatusType {
     CAHOOTS_BEFORE("CAHOOTS_BEFORE"),
     CAHOOTS_ONGOING("CAHOOTS_ONGOING"),
     CAHOOTS_OPEN("CAHOOTS_OPEN"),
-    CAHOOTS_CLOSE("CAHOOTS_CLOSE");
+    CAHOOTS_CLOSE("CAHOOTS_CLOSE"),
+    MARKET_ONGOING("MARKET_ONGOING");
     private String type;
 
     VacationStatusType(String type) {
