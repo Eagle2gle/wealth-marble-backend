395b8b7a06f3b8fdc2fb21d7675a374c1260eba3
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-17 23:33:32 +0900

    feat/#111 : 실패한 공모 처리

--- batch/src/main/java/io/eagle/job/VacationJobConfiguration.java
+++ batch/src/main/java/io/eagle/job/VacationJobConfiguration.java
@@ -1,11 +1,11 @@
 package io.eagle.job;
 
 import io.eagle.chunk.processor.VacationTransitionProcessor;
+import io.eagle.entity.Order;
 import io.eagle.entity.Vacation;
 import io.eagle.entity.type.VacationStatusType;
 import io.eagle.listener.CustomJobExecutionListener;
 import io.eagle.listener.CustomStepExecutionListener;
-import io.eagle.rowmapper.VacationMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -15,15 +15,12 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.database.JdbcCursorItemReader;
 import org.springframework.batch.item.database.JpaCursorItemReader;
 import org.springframework.batch.item.database.JpaItemWriter;
-import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.batch.item.database.builder.JpaCursorItemReaderBuilder;
 import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
@@ -36,7 +33,10 @@ public class VacationJobConfiguration {
 
     public static final String VACATION_JOB = "vacationTransitionJob";
     public static final String VACATION_TRANSITION_STEP = "vacationTransitionStep";
+    public static final String ORDER_FAIL_STEP = "orderFailStep";
+    public static final String ORDER_SUCCESS_STEP = "orderSuccessStep";
     public static final String EXPIRED_VACATION_PAGE_READER = "vacationJpaPagingItemReader";
+    public static final String FAILED_VACATION_READER = "failedVacationReader";
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -54,21 +54,21 @@ public class VacationJobConfiguration {
             .build();
     }
 
-    @Bean(VACATION_JOB)
+    @Bean
     @JobScope
     public Step vacationTransitionStep() {
         return stepBuilderFactory.get(VACATION_TRANSITION_STEP)
             .<Vacation, Vacation>chunk(chunkSize)
-            .reader(vacationPagingItemReader())
-            .processor(vacationItemProcessor())
-            .writer(vacationJpaItemWriter())
+            .reader(vacationTransitionItemReader())
+            .processor(vacationTransitionItemProcessor())
+            .writer(vacationTransitionItemWriter())
             .listener(new CustomStepExecutionListener())
             .build();
     }
 
     @Bean(VACATION_TRANSITION_STEP + "_reader")
     @StepScope
-    public JpaCursorItemReader<Vacation> vacationPagingItemReader() {
+    public JpaCursorItemReader<Vacation> vacationTransitionItemReader() {
         HashMap<String, Object> parameters = new HashMap<>();
         parameters.put("end", LocalDate.now().minusDays(1L));
         parameters.put("status", VacationStatusType.CAHOOTS_ONGOING);
@@ -82,17 +82,30 @@ public class VacationJobConfiguration {
 
     @Bean(VACATION_TRANSITION_STEP + "_processor")
     @StepScope
-    public ItemProcessor<Vacation, Vacation> vacationItemProcessor() {
+    public ItemProcessor<Vacation, Vacation> vacationTransitionItemProcessor() {
         return new VacationTransitionProcessor(dataSource);
     }
 
     @Bean(VACATION_TRANSITION_STEP + "_writer")
     @StepScope
-    public JpaItemWriter<Vacation> vacationJpaItemWriter() {
+    public JpaItemWriter<Vacation> vacationTransitionItemWriter() {
         return new JpaItemWriterBuilder<Vacation>()
             .entityManagerFactory(entityManagerFactory)
             .build();
     }
+
+    @Bean
+    @JobScope
+    public Step orderFailJob() {
+        return stepBuilderFactory.get(ORDER_FAIL_STEP + "_job")
+            .<Vacation, Order>chunk(chunkSize)
+            .reader()
+            .processor()
+            .writer()
+            .build();
+    }
+
+
 
 
 }
