b59c67145d195ac53ff2a71847e12f903c7b2937
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-17 15:34:54 +0900

    feat/#111 : vacationPagingItemReader 실행

--- batch/src/main/java/io/eagle/job/VacationJobConfiguration.java
+++ batch/src/main/java/io/eagle/job/VacationJobConfiguration.java
@@ -8,28 +8,35 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
 import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 
 @Configuration
 @RequiredArgsConstructor
 public class VacationJobConfiguration {
 
-    public static final String VACATION_TRANSITION_JOB = "vacationTransitionJob";
+    public static final String VACATION_JOB = "vacationTransitionJob";
     public static final String VACATION_TRANSITION_STEP = "vacationTransitionStep";
+    public static final String EXPIRED_VACATION_PAGE_READER = "vacationJpaPagingItemReader";
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
     private final EntityManagerFactory entityManagerFactory;
+    private final DataSource dataSource;
 
     private final int chunkSize = 10;
 
     @Bean
     public Job vacationJob() {
-        return jobBuilderFactory.get(VACATION_TRANSITION_JOB)
+        return jobBuilderFactory.get(VACATION_JOB)
             .incrementer(new RunIdIncrementer())
             .listener(new CustomJobExecutionListener())
             .start(vacationTransitionStep())
@@ -41,6 +48,19 @@ public class VacationJobConfiguration {
     public Step vacationTransitionStep() {
         return stepBuilderFactory.get(VACATION_TRANSITION_STEP)
             .<Vacation, Vacation>chunk(chunkSize)
+            .reader(vacationPagingItemReader())
+            .build();
+    }
+
+    @Bean
+    @StepScope
+    public JdbcCursorItemReader<Vacation> vacationPagingItemReader() {
+        return new JdbcCursorItemReaderBuilder<Vacation>()
+            .sql("SELECT v FROM Vacation v WHERE v.stockPeriod.end < NOW()")
+            .rowMapper(new BeanPropertyRowMapper<>(Vacation.class))
+            .fetchSize(chunkSize)
+            .dataSource(dataSource)
+            .name(EXPIRED_VACATION_PAGE_READER)
             .build();
     }
 
