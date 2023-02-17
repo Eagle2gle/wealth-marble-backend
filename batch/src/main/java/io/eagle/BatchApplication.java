eff4b7cd9d324f59f6f37bac79b438088a33ae9a
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-17 14:07:57 +0900

    chore/#111 : spring batch 실행

--- batch/src/main/java/io/eagle/BatchApplication.java
+++ batch/src/main/java/io/eagle/BatchApplication.java
@@ -1,12 +1,13 @@
 package io.eagle;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableBatchProcessing
 @SpringBootApplication
 public class BatchApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(BatchApplication.class);
     }
 }
