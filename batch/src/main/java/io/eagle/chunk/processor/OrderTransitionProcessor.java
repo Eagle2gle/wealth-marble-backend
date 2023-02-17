684ee03382fbe77562e0c8fcff675a57a38d72c2
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-18 00:06:02 +0900

    feat/#111 : OrderTransitionProcessor으로 변경

--- batch/src/main/java/io/eagle/chunk/processor/OrderTransitionProcessor.java
+++ batch/src/main/java/io/eagle/chunk/processor/OrderTransitionProcessor.java
@@ -3,6 +3,7 @@ package io.eagle.chunk.processor;
 import io.eagle.entity.Order;
 import io.eagle.entity.Vacation;
 import io.eagle.entity.type.OrderStatus;
+import io.eagle.entity.type.VacationStatusType;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -11,19 +12,25 @@ import javax.sql.DataSource;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class OrderFailProcessor implements ItemProcessor<Vacation, List<Order>> {
+public class OrderTransitionProcessor implements ItemProcessor<Vacation, List<Order>> {
 
     private JdbcTemplate jdbcTemplate;
 
-    public OrderFailProcessor(DataSource dataSource) {
+    public OrderTransitionProcessor(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     @Override
     public List<Order> process(Vacation item) throws Exception {
         return findAllOrderByVacation(item.getId()).stream().map(order -> {
-            order.setStatus(OrderStatus.FAILED);
-            return order;
+            if (item.getStatus() == VacationStatusType.CAHOOTS_CLOSE) {
+                order.setStatus(OrderStatus.FAILED);
+                return order;
+            } else if (item.getStatus() == VacationStatusType.MARKET_ONGOING) {
+                order.setStatus(OrderStatus.DONE);
+                return order;
+            }
+            return null;
         }).collect(Collectors.toList());
     }
 
