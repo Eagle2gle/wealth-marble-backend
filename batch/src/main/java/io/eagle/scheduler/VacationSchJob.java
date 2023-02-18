e341770f5ce78f644650678cb7305901daf78716
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-18 18:22:55 +0900

    feat/#111 : VacationSchJob 설정

---
+++ batch/src/main/java/io/eagle/scheduler/VacationSchJob.java
@@ -0,0 +1,33 @@
+package io.eagle.scheduler;
+
+import lombok.SneakyThrows;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class VacationSchJob extends QuartzJobBean {
+
+    @Autowired
+    private Job vacationJob;
+
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @SneakyThrows
+    @Override
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+        JobParameters jobParameters = new JobParametersBuilder().
+            addLong("id", new Date().getTime())
+            .toJobParameters();
+        jobLauncher.run(vacationJob, jobParameters);
+    }
+}
