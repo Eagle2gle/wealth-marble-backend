50401fc13d8f5b39fcf49a0e20ace1ba261daa80
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-18 22:03:04 +0900

    refactor/#111 : @RequiredArgsConstructor 사용

--- batch/src/main/java/io/eagle/chunk/writer/OrderTransitionItemWriter.java
+++ batch/src/main/java/io/eagle/chunk/writer/OrderTransitionItemWriter.java
@@ -1,18 +1,16 @@
 package io.eagle.chunk.writer;
 
 import io.eagle.entity.Order;
+import lombok.RequiredArgsConstructor;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.JpaItemWriter;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 public class OrderTransitionItemWriter implements ItemWriter<List<Order>> {
 
-    private JpaItemWriter<Order> jpaItemWriter;
-
-    public OrderTransitionItemWriter(JpaItemWriter<Order> jpaItemWriter) {
-        this.jpaItemWriter = jpaItemWriter;
-    }
+    private final JpaItemWriter<Order> jpaItemWriter;
 
     @Override
     public void write(List<? extends List<Order>> items) throws Exception {
--- batch/src/main/java/io/eagle/chunk/writer/TransactionAssignItemWriter.java
+++ batch/src/main/java/io/eagle/chunk/writer/TransactionAssignItemWriter.java
@@ -1,18 +1,16 @@
 package io.eagle.chunk.writer;
 
 import io.eagle.entity.Transaction;
+import lombok.RequiredArgsConstructor;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.JpaItemWriter;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 public class TransactionAssignItemWriter implements ItemWriter<List<Transaction>> {
 
-    private JpaItemWriter<Transaction> jpaItemWriter;
-
-    public TransactionAssignItemWriter(JpaItemWriter<Transaction> jpaItemWriter) {
-        this.jpaItemWriter = jpaItemWriter;
-    }
+    private final JpaItemWriter<Transaction> jpaItemWriter;
 
     @Override
     public void write(List<? extends List<Transaction>> items) throws Exception {
