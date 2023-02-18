86f288bcc3807f776e8a1316f0d86f2cd4b8f150
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-18 18:23:05 +0900

    feat/#111 : VacationJobRunner 수행

---
+++ batch/src/main/java/io/eagle/scheduler/VacationJobRunner.java
@@ -0,0 +1,30 @@
+package io.eagle.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.Trigger;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+
+@Component
+@RequiredArgsConstructor
+public class VacationJobRunner extends JobRunner{
+
+    private final Scheduler scheduler;
+
+    @Override
+    protected void doRun(ApplicationArguments args) {
+        JobDetail jobDetail = buildJobDetail(VacationSchJob.class, "vacationJob", "batch", new HashMap());
+        Trigger trigger = buildJobTrigger("0 09 * * * 0");
+
+        try{
+            scheduler.scheduleJob(jobDetail, trigger);
+        } catch (SchedulerException e) {
+            e.printStackTrace();
+        }
+    }
+}
