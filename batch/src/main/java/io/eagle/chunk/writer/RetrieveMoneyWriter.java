5005f05df4862fc8b0486f04291503c4f9f39dd2
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-19 20:15:18 +0900

    feat/#113 : RetrieveMoneyWriter 제작

---
+++ batch/src/main/java/io/eagle/chunk/writer/RetrieveMoneyWriter.java
@@ -0,0 +1,24 @@
+package io.eagle.chunk.writer;
+
+import io.eagle.domain.RetrieveMoneyVO;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+
+import java.util.List;
+
+public class RetrieveMoneyWriter implements ItemWriter<List<RetrieveMoneyVO>> {
+
+    private JdbcBatchItemWriter jdbcBatchItemWriter;
+
+    public RetrieveMoneyWriter(JdbcBatchItemWriter jdbcBatchItemWriter) {
+        this.jdbcBatchItemWriter = jdbcBatchItemWriter;
+    }
+
+    @Override
+    public void write(List<? extends List<RetrieveMoneyVO>> items) throws Exception {
+        for (List<RetrieveMoneyVO> retrieveMoneyVOS: items) {
+            jdbcBatchItemWriter.setSql("UPDATE user SET cash = cash + :addCash WHERE id = :userID");
+            jdbcBatchItemWriter.write(retrieveMoneyVOS);
+        }
+    }
+}
