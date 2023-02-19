986337d0978ce1dfcfffdc323ad69473b4dd105e
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-19 20:15:09 +0900

    feat/#113 : DeleteContestParticipationItemWriter 제작

---
+++ batch/src/main/java/io/eagle/chunk/writer/DeleteContestParticipationItemWriter.java
@@ -0,0 +1,24 @@
+package io.eagle.chunk.writer;
+
+import io.eagle.domain.RetrieveMoneyVO;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+
+import java.util.List;
+
+public class DeleteContestParticipationItemWriter implements ItemWriter<List<RetrieveMoneyVO>> {
+
+    private JdbcBatchItemWriter jdbcBatchItemWriter;
+
+    public DeleteContestParticipationItemWriter(JdbcBatchItemWriter jdbcBatchItemWriter) {
+        this.jdbcBatchItemWriter = jdbcBatchItemWriter;
+    }
+
+    @Override
+    public void write(List<? extends List<RetrieveMoneyVO>> items) throws Exception {
+        for (List<RetrieveMoneyVO> retrieveMoneyVOS: items) {
+            jdbcBatchItemWriter.setSql("DELETE FROM contest_participation WHERE id = :contestParticipationId");
+            jdbcBatchItemWriter.write(retrieveMoneyVOS);
+        }
+    }
+}
