9728abaab8043540288a417e54ad1ce4da5c5dd8
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-20 17:53:11 +0900

    test/#118 : 내_관심상품_가져오기 테스트 작성

--- api/src/test/java/io/eagle/domain/interest/service/InterestServiceTest.java
+++ api/src/test/java/io/eagle/domain/interest/service/InterestServiceTest.java
@@ -2,18 +2,23 @@ package io.eagle.domain.interest.service;
 
 import io.eagle.common.TestUtil;
 import io.eagle.domain.interest.dto.InterestDto;
+import io.eagle.domain.interest.dto.InterestInfoDto;
 import io.eagle.domain.interest.repository.InterestRepository;
 import io.eagle.domain.vacation.repository.VacationRepository;
 import io.eagle.entity.Interest;
 import io.eagle.entity.User;
 import io.eagle.entity.Vacation;
+import io.eagle.entity.type.VacationStatusType;
 import io.eagle.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -30,6 +35,31 @@ public class InterestServiceTest {
 
     @InjectMocks
     private InterestService interestService;
+
+    @Test
+    @DisplayName("내_관심상품_가져오기")
+    void getAllInterest() {
+        // given
+        TestUtil testUtil = new TestUtil();
+        User user = testUtil.createUser("anonymous", "anonymous@email.com");
+        User marketUser = testUtil.createUser("market", "market@email.com");
+        Vacation vacation = testUtil.createVacation(user);
+        vacation.setStatus(VacationStatusType.MARKET_ONGOING);
+        Interest interest = testUtil.createInterest(marketUser, vacation);
+        Pageable pageable = Pageable.ofSize(10);
+        String type = "market";
+
+        // when
+        when(interestRepository.findInterestByUser(marketUser, pageable)).thenReturn(List.of(interest));
+        List<InterestInfoDto> interestDtos = interestService.getAllInterest(type, marketUser, pageable);
+
+        // then
+        InterestInfoDto infoDto = interestDtos.get(0);
+        assertEquals(infoDto.getTitle(), vacation.getTitle());
+        assertEquals(infoDto.getShortDescription(), vacation.getShortDescription());
+        assertEquals(infoDto.getLocation(), vacation.getLocation());
+        assertEquals(infoDto.getPicture(), vacation.getPictureList().get(0));
+    }
 
     @Test
     @DisplayName("관심상품_생성하기")
