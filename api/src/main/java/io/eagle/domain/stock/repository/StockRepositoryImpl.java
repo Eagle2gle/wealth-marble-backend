b233254d69b1c9dbb67c244fe9670a15aea4d070
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-22 20:52:40 +0900

    feat/#43 : StockRepositoryImpl 구현

---
+++ api/src/main/java/io/eagle/domain/stock/repository/StockRepositoryImpl.java
@@ -0,0 +1,26 @@
+package io.eagle.domain.stock.repository;
+
+import com.querydsl.jpa.JPQLQueryFactory;
+import io.eagle.domain.stock.dto.QStockInfoDto;
+import io.eagle.domain.stock.dto.StockInfoDto;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import static io.eagle.entity.QStock.*;
+
+@RequiredArgsConstructor
+public class StockRepositoryImpl implements StockRepositoryCustom {
+
+    private final JPQLQueryFactory jpqlQueryFactory;
+
+    @Override
+    public List<StockInfoDto> getTotalStockValueByUser(Long userId) {
+        return jpqlQueryFactory
+            .select(new QStockInfoDto(stock.price, stock.amount))
+            .from(stock)
+            .where(stock.user.id.eq(userId))
+            .groupBy(stock.vacation.id)
+            .fetch();
+    }
+}
