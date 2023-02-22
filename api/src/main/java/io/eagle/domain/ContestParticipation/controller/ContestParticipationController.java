4b4ea5cc662fcbe648041e2fa500de808d26f9c2
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-22 22:26:11 +0900

    feat/#45 : /api/auth/contestParticipation/me

--- api/src/main/java/io/eagle/domain/ContestParticipation/controller/ContestParticipationController.java
+++ api/src/main/java/io/eagle/domain/ContestParticipation/controller/ContestParticipationController.java
@@ -1,28 +1,37 @@
 package io.eagle.domain.ContestParticipation.controller;
 
 
+import io.eagle.auth.AuthDetails;
 import io.eagle.common.ApiResponse;
 import io.eagle.domain.ContestParticipation.service.ContestParticipationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/cahoots")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class ContestParticipationController {
     private final ContestParticipationService contestParticipationService;
 
-    @PostMapping("/{cahootsId}")
+    @PostMapping("/cahoots/{cahootsId}")
     public ApiResponse participateCahoots (@PathVariable("cahootsId") Long cahootsId, @RequestBody Map<String,Integer> param){
         contestParticipationService.participate(cahootsId, param.get("stocks"));
         return ApiResponse.createSuccessWithNoContent();
     }
 
-    @GetMapping(value = "/{cahootsId}", params = "info=history")
+    @GetMapping(value = "/cahoots/{cahootsId}", params = "info=history")
     public ApiResponse getCahootsHistoryInfo(@PathVariable("cahootsId") Long cahootsId){
         return ApiResponse.createSuccess(contestParticipationService.getHistory(cahootsId));
+    }
+
+    @GetMapping("/auth/contestParticipation/me")
+    public ApiResponse getMyContestParticipation(
+        @AuthenticationPrincipal AuthDetails authDetails
+        ) {
+        return ApiResponse.createSuccess(contestParticipationService.getMyContestParticipation(authDetails.getUser()));
     }
 }
