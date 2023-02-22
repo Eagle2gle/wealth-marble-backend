457898fde74d33151bb7bc0709436c58432823e8
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-22 20:52:50 +0900

    feat/#43 : UserInfoDto 제작

---
+++ api/src/main/java/io/eagle/domain/user/dto/response/UserInfoDto.java
@@ -0,0 +1,27 @@
+package io.eagle.domain.user.dto.response;
+
+import io.eagle.entity.User;
+import io.eagle.entity.type.ProviderType;
+import io.eagle.entity.type.Role;
+import lombok.Getter;
+
+@Getter
+public class UserInfoDto {
+
+    private String username;
+    private Integer cash;
+    private Integer value;
+    private String email;
+    private ProviderType providerType;
+    private Role role;
+
+    public UserInfoDto(Integer value, User user) {
+        this.username = user.getNickname();
+        this.cash = user.getCash();
+        this.value = value;
+        this.email = user.getEmail();
+        this.providerType = user.getProviderType();
+        this.role = user.getRole();
+    }
+
+}
