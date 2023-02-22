ca395d8d0f0765cbf3edee908bcf3fed688a9670
NaayoungKwon <kny8092@gmail.com>
2023-02-22 23:51:10 +0900

    feat/#123 : price info job2 생성
     - 전날 없었던 거래 마켓은 이틀 전의 정보를 기반으로 price info 생성

---
+++ batch/src/main/java/io/eagle/common/BatchConstant.java
@@ -0,0 +1,11 @@
+package io.eagle.common;
+
+public final class BatchConstant {
+    public static final Integer CHUNK_SIZE = 10;
+    public static final String PRICE_JOB = "priceInfoJob";
+    public static final String TRANSACTIONS_SUMMARY_STEP = "transactionsSummaryStep";
+    public static final String TRANSACTIONS_SUMMARY_READER = "transactionsSummaryItemReader";
+
+    public static final String TRANSFER_LASTDAY_PRICEINFO_STEP = "transferLastdayPriceInfoStep";
+    public static final String TRANSFER_LASTDAY_PRICEINFO_READER = "transferLastdayPriceInfoItemReader";
+}
