df98356ceb7668d3f68ffabaa9c3172f0f715459
NaayoungKwon <kny8092@gmail.com>
2023-02-24 01:42:52 +0900

    setting : createdAt, updatedAt 자동 삽입

--- batch/src/main/java/io/eagle/BatchApplication.java
+++ batch/src/main/java/io/eagle/BatchApplication.java
@@ -3,8 +3,10 @@ package io.eagle;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableBatchProcessing
+@EnableJpaAuditing
 @SpringBootApplication
 public class BatchApplication {
     public static void main(String[] args) {
