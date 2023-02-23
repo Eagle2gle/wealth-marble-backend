9e64522ea06f8d40d0acd5ece3083acd1b353a74
NaayoungKwon <kny8092@gmail.com>
2023-02-24 01:46:04 +0900

    feat/#123 : processor에서 priceInfoVO->priceInfo 로 맵핑

--- batch/src/main/java/io/eagle/chunk/processor/CopyPriceInfoProcessor.java
+++ batch/src/main/java/io/eagle/chunk/processor/CopyPriceInfoProcessor.java
@@ -1,21 +1,16 @@
 package io.eagle.chunk.processor;
 
+import io.eagle.domain.PriceInfoVO;
 import io.eagle.entity.PriceInfo;
+import io.eagle.entity.Vacation;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.jdbc.core.JdbcTemplate;
 
-import javax.sql.DataSource;
-
-public class TransferLastdayInfoProcessor implements ItemProcessor<PriceInfo, PriceInfo> {
-    private JdbcTemplate jdbcTemplate;
-
-    public TransferLastdayInfoProcessor(DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
-    }
+public class CopyPriceInfoProcessor implements ItemProcessor<PriceInfoVO, PriceInfo> {
 
     @Override
-    public PriceInfo process(PriceInfo lastPriceInfo) throws Exception {
+    public PriceInfo process(PriceInfoVO lastPriceInfo) throws Exception {
         return PriceInfo.builder()
+                .vacation(Vacation.builder().id(lastPriceInfo.getVacationId()).build())
                 .highPrice(lastPriceInfo.getStandardPrice())
                 .lowPrice(lastPriceInfo.getStandardPrice())
                 .standardPrice(lastPriceInfo.getStandardPrice())
