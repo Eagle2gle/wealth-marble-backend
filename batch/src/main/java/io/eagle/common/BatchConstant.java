82055083233bb4cc00ec83c1fcc3079837b68270
NaayoungKwon <kny8092@gmail.com>
2023-02-24 01:42:01 +0900

    refactor : constant 모으기

--- batch/src/main/java/io/eagle/common/BatchConstant.java
+++ batch/src/main/java/io/eagle/common/BatchConstant.java
@@ -1,7 +1,14 @@
 package io.eagle.common;
 
 public final class BatchConstant {
-    public static final Integer CHUNK_SIZE = 10;
+    public static final Integer CHUNK_SIZE = 1;
+
+    public static final String VACATION_JOB = "vacationTransitionJob";
+    public static final String VACATION_TRANSITION_STEP = "vacationTransitionStep";
+    public static final String CREATE_STOCK = "createStock";
+    public static final String RETRIEVE_MONEY = "retrieveMoney";
+    public static final String EXPIRED_VACATION_PAGE_READER = "vacationJpaPagingItemReader";
+    public static final String CREATE_STOCK_READER = "createStockReader";
     public static final String PRICE_JOB = "priceInfoJob";
     public static final String TRANSACTIONS_SUMMARY_STEP = "transactionsSummaryStep";
     public static final String TRANSACTIONS_SUMMARY_READER = "transactionsSummaryItemReader";
