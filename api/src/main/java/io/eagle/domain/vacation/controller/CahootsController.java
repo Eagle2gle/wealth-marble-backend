6bba1c6d9bbd58eb440d27cd0f04e90334e0b858
NaayoungKwon <kny8092@gmail.com>
2023-02-25 22:09:03 +0900

    feat/#82 : 공모 생성 auth 적용

--- api/src/main/java/io/eagle/domain/vacation/controller/CahootsController.java
+++ api/src/main/java/io/eagle/domain/vacation/controller/CahootsController.java
@@ -1,5 +1,6 @@
 package io.eagle.domain.vacation.controller;
 
+import io.eagle.auth.AuthDetails;
 import io.eagle.common.ApiResponse;
 import io.eagle.domain.vacation.dto.*;
 import io.eagle.domain.vacation.dto.request.CreateCahootsDto;
@@ -7,53 +8,54 @@ import io.eagle.domain.vacation.dto.response.ImminentInfoDto;
 import io.eagle.entity.type.VacationStatusType;
 import io.eagle.domain.vacation.service.CahootsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/api/cahoots")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class CahootsController {
     private final CahootsService cahootsService;
 
-    @PostMapping
-    public ApiResponse createCahoots (@Valid CreateCahootsDto createCahootsDto){
-        cahootsService.create(createCahootsDto);
+    @PostMapping("/auth/cahoots")
+    public ApiResponse createCahoots (@Valid CreateCahootsDto createCahootsDto, @AuthenticationPrincipal AuthDetails authDetails){
+        cahootsService.create(createCahootsDto, authDetails.getUser());
         return ApiResponse.createSuccessWithNoContent();
     }
 
-    @GetMapping(value = "/{cahootsId}", params = "info=detail")
+    @GetMapping(value = "/cahoots/{cahootsId}", params = "info=detail")
     public ApiResponse getCahootsDetailInfo(@PathVariable("cahootsId") Long cahootsId){
         return ApiResponse.createSuccess(cahootsService.getDetail(cahootsId));
     }
 
-    @GetMapping(params = "status=ongoing")
+    @GetMapping(value="/cahoots", params = "status=ongoing")
     public ApiResponse getBreifCahootsInfo(@Valid InfoConditionDto infoConditionDto){
         infoConditionDto.setTypes(new VacationStatusType[]{VacationStatusType.CAHOOTS_ONGOING});
         return ApiResponse.createSuccess(cahootsService.getBreifList(infoConditionDto));
     }
 
-    @GetMapping(params = "status=ended")
+    @GetMapping(value="/cahoots", params = "status=ended")
     public ApiResponse getEndedBreifCahootsInfo(){
         VacationStatusType[] types = new VacationStatusType[]{VacationStatusType.CAHOOTS_CLOSE, VacationStatusType.CAHOOTS_OPEN};
         InfoConditionDto infoConditionDto = InfoConditionDto.builder().types(types).page(0).build();
         return ApiResponse.createSuccess(cahootsService.getBreifList(infoConditionDto));
     }
 
-    @GetMapping(params = "status=ending-soon")
+    @GetMapping(value="/cahoots",params = "status=ending-soon")
     public ApiResponse getEndedSoonBreifCahootsInfo(){
         VacationStatusType[] types = new VacationStatusType[]{VacationStatusType.CAHOOTS_ONGOING};
         InfoConditionDto infoConditionDto = InfoConditionDto.builder().types(types).page(0).build();
         return ApiResponse.createSuccess(cahootsService.getBreifV2List(infoConditionDto));
     }
 
-    @GetMapping(value= "/mini", params = "status=ending-soon")
+    @GetMapping(value= "/cahoots/mini", params = "status=ending-soon")
     public ApiResponse getMostImminentEndedSoon(){
         return ApiResponse.createSuccess(cahootsService.getMostImminentCahoots());
     }
 
-    @GetMapping("/recent")
+    @GetMapping("/cahoots/recent")
     public ApiResponse getlatestCahootsInfo(){
         return ApiResponse.createSuccess(cahootsService.getLatestsList());
     }
