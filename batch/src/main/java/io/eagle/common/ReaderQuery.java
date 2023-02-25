133e248920aec0ba3a62049f00cc171dfc21978e
NaayoungKwon <kny8092@gmail.com>
2023-02-25 19:02:20 +0900

    refactor/#123 : query 개선 및 오타 수정

--- batch/src/main/java/io/eagle/common/ReaderQuery.java
+++ batch/src/main/java/io/eagle/common/ReaderQuery.java
@@ -17,19 +17,14 @@ public enum ReaderQuery {
             "     order by t.created_at desc\n" +
             "     limit 1\n" +
             "    ) as standard_price,\n" +
-            "    (select standard_price\n" +
-            "     from price_info as p\n" +
-            "     where DATE_FORMAT(p.created_at, '%Y-%m-%d') = :twoDaysAgo and p.vacation_id = vacation_id\n" +
-            "     limit 1\n" +
-            "    ) as start_price,\n" +
+            "    pi.standard_price as start_price,\n" +
             "    sum(TR.amount) as transaction_amount,\n" +
-            "    (select sum(t.amount * t.price)\n" +
-            "     from transaction as t\n" +
-            "     where DATE_FORMAT(created_at, '%Y-%m-%d') = :lastday and t.vacation_id = TR.vacation_id\n" +
-            "    ) as transaction_money\n" +
-            "from transaction as TR\n" +
-            "where DATE_FORMAT(created_at, '%Y-%m-%d') = :lastday\n" +
-            "group by vacation_id"),
+            "    sum(TR.amount * TR.price) as transaction_money\n" +
+            "from transaction as TR \n" +
+            "   left join price_info pi\n" +
+            "    on TR.vacation_id = pi.vacation_id and DATE_FORMAT(pi.created_at, '%Y-%m-%d') = :twoDaysAgo\n" +
+            "where DATE_FORMAT(TR.created_at, '%Y-%m-%d') = :lastday\n" +
+            "group by TR.vacation_id"),
 
     SelectNoTransactionsQuery("select\n" +
             "    p.* \n" +
@@ -43,8 +38,9 @@ public enum ReaderQuery {
             "         where DATE_FORMAT(created_at, '%Y-%m-%d') = :lastday\n" +
             "         group by vacation_id\n" +
             "         )\n" +
-            "     )\n" +
-            "    and DATE_FORMAT(p.created_at, '%Y-%m-%d') = :twoDaysAgo");
+            "     ) " +
+            "    and " +
+            "       DATE_FORMAT(p.created_at, '%Y-%m-%d') = :twoDaysAgo");
 
     public final String query;
 }
