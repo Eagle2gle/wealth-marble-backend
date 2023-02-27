ea595d213eb724f1943261e0b3d03b9029cc9257
CEOJINSUNG <jinsung1048@gmail.com>
2023-02-27 13:27:44 +0900

    feat/#117 : findOneByVacationId 작성

--- api/src/main/java/io/eagle/domain/PriceInfo/repository/PriceInfoRepository.java
+++ api/src/main/java/io/eagle/domain/PriceInfo/repository/PriceInfoRepository.java
@@ -2,6 +2,12 @@ package io.eagle.domain.PriceInfo.repository;
 
 import io.eagle.entity.PriceInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PriceInfoRepository extends JpaRepository<PriceInfo, Long>, PriceInfoRepositoryCustom {
+
+    @Query(value = "SELECT * FROM price_info p WHERE p.vacationId = :vacationId ORDER BY p.id LIMIT 1", nativeQuery = true)
+    PriceInfo findOneByVacationId(@Param("vacationId") Long vacationId);
+
 }
