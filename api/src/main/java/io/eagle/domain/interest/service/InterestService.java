b385ca0f1f0da85fe253307fdaa602c24517a24a
CEOJINSUNG <jinsung1048@gmail.com>
2023-03-02 13:36:23 +0900

    fix/#140 : Vacation Picture null Error

--- api/src/main/java/io/eagle/domain/interest/service/InterestService.java
+++ api/src/main/java/io/eagle/domain/interest/service/InterestService.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -33,7 +34,7 @@ public class InterestService {
                 return new InterestInfoDto(interest.getVacation());
             }
             return null;
-        }).collect(Collectors.toList());
+        }).filter(Objects::nonNull).collect(Collectors.toList());
     }
 
     public Boolean isUserInterest(User user) {
