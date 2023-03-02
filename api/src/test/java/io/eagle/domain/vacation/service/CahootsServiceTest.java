ad4c6fb68cfd7422889a92fa0679b3b590f0e7cf
NaayoungKwon <kny8092@gmail.com>
2023-03-02 20:54:54 +0900

    test/#49 : 공모생성 테스트

--- api/src/test/java/io/eagle/domain/vacation/service/CahootsServiceTest.java
+++ api/src/test/java/io/eagle/domain/vacation/service/CahootsServiceTest.java
@@ -3,9 +3,12 @@ package io.eagle.domain.vacation.service;
 import io.eagle.common.TestUtil;
 import io.eagle.domain.ContestParticipation.repository.ContestParticipationRepository;
 import io.eagle.domain.interest.repository.InterestRepository;
+import io.eagle.domain.vacation.dto.request.CreateCahootsDto;
 import io.eagle.domain.vacation.dto.response.DetailCahootsDto;
 import io.eagle.domain.vacation.repository.VacationRepository;
 import io.eagle.entity.*;
+import io.eagle.entity.type.ThemeBuildingType;
+import io.eagle.entity.type.ThemeLocationType;
 import io.eagle.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,7 +18,9 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -40,6 +45,38 @@ public class CahootsServiceTest {
     private InterestRepository interestRepository;
 
     private final TestUtil testUtil = new TestUtil();
+
+    @Test
+    @DisplayName("공모 생성 확인")
+    @Transactional
+    public void createCahoots() {
+        User owner = testUtil.createUser("anonymous", "anonymous@email.com");
+        userRepository.save(owner);
+        CreateCahootsDto createCahootsDto = CreateCahootsDto.builder()
+                .title("준호네 집 테스트용")
+                .description("우리집 준호를 다같이 이용합니다. 준호는 공공재입니다.")
+                .expectedMonth(12)
+                .expectedTotalCost(100000L)
+                .country("대한민국")
+                .location("경기도 성남시 분당구")
+                .shortDescription("우리집 그 준호입니다.")
+                .stockStart(LocalDate.of(2024,2,23))
+                .stockEnd(LocalDate.of(2024,4,23))
+                .stockNum(10)
+                .stockPrice(12000L)
+                .themeBuilding(ThemeBuildingType.GUEST_HOUSE)
+                .themeLocation(ThemeLocationType.DOWNTOWN)
+                .expectedRateOfReturn(120)
+                .build();
+        cahootsService.create(createCahootsDto, owner);
+        Vacation vacation = vacationRepository.findByTitle(createCahootsDto.getTitle()).get();
+
+        assertEquals(createCahootsDto.getDescription(), vacation.getDescription());
+        assertEquals(createCahootsDto.getShortDescription(), vacation.getShortDescription());
+        assertEquals(createCahootsDto.getStockPrice(), vacation.getStock().getPrice());
+        assertEquals(owner.getEmail(), vacation.getUser().getEmail());
+
+    }
 
     @Test
     @DisplayName("공모 상세 정보")
