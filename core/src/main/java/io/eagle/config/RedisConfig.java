155f010e5e680d648381dae3ba2fb3efbe798443
CEOJINSUNG <jinsung1048@gmail.com>
2023-03-05 18:03:23 +0900

    feat/#155 : Redisson 설정하기

--- core/src/main/java/io/eagle/config/RedisConfig.java
+++ core/src/main/java/io/eagle/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package io.eagle.config;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +19,15 @@ public class RedisConfig {
 
     @Value("${spring.redis.port}")
     private Integer port;
+
+    private static final String REDISSON_HOST_PREFIX = "redis://";
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + host + ":" + port);
+        return Redisson.create(config);
+    }
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
