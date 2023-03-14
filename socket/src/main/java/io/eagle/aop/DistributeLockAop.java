6adca9b923e01cea7a38e561b974abd14edbbe61
김진성 Kim Jinsung <55318896+CEOJINSUNG@users.noreply.github.com>
2023-03-14 19:15:39 +0900

    feat/#155 : DistributeLockAop 제작

---
+++ socket/src/main/java/io/eagle/aop/DistributeLockAop.java
@@ -0,0 +1,51 @@
+package io.eagle.aop;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class DistributeLockAop {
+
+    private static final String REDISSON_KEY_PREFIX = "RLOCK_";
+    private final RedissonClient redissonClient;
+    private final AopForTransaction aopForTransaction;
+
+    @Around("@annotation(io.eagle.aop.DistributeLock)")
+    public Object lock(final ProceedingJoinPoint joinPoint) throws Throwable {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        DistributeLock distributeLock = method.getAnnotation(DistributeLock.class);
+        String key = REDISSON_KEY_PREFIX + CustomSpringELParser.getDynamicValue(signature.getParameterNames(), joinPoint.getArgs(), distributeLock.key());
+        RLock rLock = redissonClient.getLock(key);
+
+        try {
+            boolean available = rLock.tryLock(distributeLock.waitTime(), distributeLock.leaseTime(), distributeLock.timeUnit());
+
+            if (!available) {
+                return false;
+            }
+
+            log.info("get lock success {}", key);
+            return aopForTransaction.proceed(joinPoint);
+        } catch (Exception e) {
+            Thread.currentThread().interrupt();
+            throw new InterruptedException();
+        } finally {
+            rLock.unlock();
+        }
+    }
+
+
+}
