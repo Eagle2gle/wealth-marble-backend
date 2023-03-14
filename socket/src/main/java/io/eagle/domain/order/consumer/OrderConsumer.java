6639ff1a8e1db88e2443041f3351743d708632d1
김진성 Kim Jinsung <55318896+CEOJINSUNG@users.noreply.github.com>
2023-03-14 16:11:12 +0900

    refactor/#155 : Message Refactor 변경 적용

--- socket/src/main/java/io/eagle/domain/order/consumer/OrderConsumer.java
+++ socket/src/main/java/io/eagle/domain/order/consumer/OrderConsumer.java
@@ -1,7 +1,7 @@
 package io.eagle.domain.order.consumer;
 
 import io.eagle.common.KafkaConstants;
-import io.eagle.domain.order.dto.response.BroadcastMessageDto;
+import io.eagle.domain.order.dto.response.BroadcastStockDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -14,7 +14,7 @@ public class OrderConsumer {
     private final SimpMessagingTemplate template;
 
     @KafkaListener( topics = KafkaConstants.KAFKA_TOPIC, groupId = KafkaConstants.GROUP_ID )
-    public void listen(BroadcastMessageDto message){ // kafka listener에서 듣고있음
-        template.convertAndSend("/topic/market/" + message.getMarketId(), message); // topic/market/{마켓아이디}를 듣고있는 client에 전송
+    public void listen(BroadcastStockDto stock){ // kafka listener에서 듣고있음
+        template.convertAndSend("/topic/market/" + stock.getMarketId(), stock); // topic/market/{마켓아이디}를 듣고있는 client에 전송
     }
 }
--- socket/src/main/java/io/eagle/domain/order/controller/OrderController.java
+++ socket/src/main/java/io/eagle/domain/order/controller/OrderController.java
@@ -2,8 +2,8 @@ package io.eagle.domain.order.controller;
 
 import io.eagle.common.KafkaConstants;
 import io.eagle.domain.order.dto.response.ErrorDto;
-import io.eagle.domain.order.dto.request.MessageDto;
-import io.eagle.domain.order.dto.response.BroadcastMessageDto;
+import io.eagle.domain.order.dto.request.StockDto;
+import io.eagle.domain.order.dto.response.BroadcastStockDto;
 import io.eagle.domain.order.service.OrderService;
 import io.eagle.exception.SocketException;
 import lombok.RequiredArgsConstructor;
@@ -18,22 +18,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Slf4j
 public class OrderController {
-    private final KafkaTemplate<String, BroadcastMessageDto> kafkaTemplate;
+    private final KafkaTemplate<String, BroadcastStockDto> kafkaTemplate;
     private final OrderService orderService;
 
     @MessageMapping("/purchase") //   url : "order/purchase" 로 들어오는 정보 처리
-    public void purchase(MessageDto message){
-        BroadcastMessageDto broadcastMessageDto = orderService.purchaseMarket(message);
-        log.info("[STOMP Producer] user purchase market {} price : {}, amount : {}, left : {}",message.getMarketId(),message.getPrice(), message.getAmount(), broadcastMessageDto.getAmount());
+    public void purchase(StockDto stockDto){
+        BroadcastStockDto broadcastMessageDto = orderService.purchaseMarket(stockDto);
+        log.info("[STOMP Producer] user purchase market {} price : {}, amount : {}, left : {}", stockDto.getMarketId(), stockDto.getPrice(), stockDto.getAmount(), broadcastMessageDto.getAmount());
         kafkaTemplate.send(KafkaConstants.KAFKA_TOPIC, broadcastMessageDto);
     }
 
-//    @MessageMapping("/sale") //   url : "order/sale"
-//    public void sale(MessageDto message){
-//        BroadcastMessageDto broadcastMessageDto = orderService.sellMarket(message);
-//        log.info("[STOMP Producer] user sell market {} price : {}, amount : {}, left : {}",message.getMarketId(),message.getPrice(), message.getAmount());
-//        kafkaTemplate.send(KafkaConstants.KAFKA_TOPIC, broadcastMessageDto);
-//    }
+    @MessageMapping("/sale") //   url : "order/sale"
+    public void sale(StockDto message){
+        BroadcastStockDto broadcastMessageDto = orderService.sellMarket(message);
+        log.info("[STOMP Producer] user sell market {} price : {}, amount : {}, left : {}",message.getMarketId(),message.getPrice(), message.getAmount());
+        kafkaTemplate.send(KafkaConstants.KAFKA_TOPIC, broadcastMessageDto);
+    }
 
     @MessageExceptionHandler
     @SendToUser("/queue/errors") // '/user/queue/errors' 를 구독하고 있어야함
