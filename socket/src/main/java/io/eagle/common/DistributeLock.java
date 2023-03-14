55144999205d142900ec60e16155a7983a21c93e
김진성 Kim Jinsung <55318896+CEOJINSUNG@users.noreply.github.com>
2023-03-14 16:44:49 +0900

    feat/#155 : @DistributeLock 구현

---
+++ socket/src/main/java/io/eagle/common/DistributeLock.java
@@ -0,0 +1,18 @@
+package io.eagle.common;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.concurrent.TimeUnit;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DistributeLock {
+
+    String key(); // 락의 이름
+    TimeUnit timeUnit() default TimeUnit.SECONDS; // 시간단위
+    long waitTime() default 5L; // 락을 임대하기 위한 대기 시간
+    long leaseTime() default 3L; // 락을 임대하는 시간
+
+}
