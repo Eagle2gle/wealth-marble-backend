7264196d5bdd79439529d5a7cb5776c60c951733
NaayoungKwon <kny8092@gmail.com>
2023-03-15 21:20:42 +0900

    feat/#167 : 주문 리스트 전달 repository 작성

--- api/src/main/java/io/eagle/domain/order/repository/OrderRepositoryCustom.java
+++ api/src/main/java/io/eagle/domain/order/repository/OrderRepositoryCustom.java
@@ -1,12 +1,15 @@
 package io.eagle.domain.order.repository;
 
+import io.eagle.domain.order.dto.response.AvailableOrderDto;
 import io.eagle.entity.Order;
+import io.eagle.entity.Transaction;
 import io.eagle.entity.User;
+import io.eagle.entity.type.OrderType;
 
 import java.util.List;
 
 public interface OrderRepositoryCustom {
 
     List<Order> findAllByUser(User user);
-
+    List<AvailableOrderDto> findTop5ByVacationIdAndOrderTypeOrderByPrice(Long id, OrderType type);
 }
