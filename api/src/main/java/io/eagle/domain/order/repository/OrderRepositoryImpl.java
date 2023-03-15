415b8f6cd605e7e3edbd43761f41831529c79768
NaayoungKwon <kny8092@gmail.com>
2023-03-15 21:20:42 +0900

    feat/#167 : 주문 리스트 전달 repository 작성

--- api/src/main/java/io/eagle/domain/order/repository/OrderRepositoryImpl.java
+++ api/src/main/java/io/eagle/domain/order/repository/OrderRepositoryImpl.java
@@ -1,11 +1,16 @@
 package io.eagle.domain.order.repository;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.JPQLQueryFactory;
+import io.eagle.domain.order.dto.response.AvailableOrderDto;
 import io.eagle.entity.Order;
 import io.eagle.entity.User;
+import io.eagle.entity.type.OrderType;
 import lombok.RequiredArgsConstructor;
 
 import static io.eagle.entity.QOrder.order;
+import static io.eagle.entity.type.OrderStatus.ONGOING;
+import static io.eagle.entity.type.OrderType.SELL;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,5 +28,21 @@ public class OrderRepositoryImpl implements OrderRepositoryCustom {
             .fetchAll()
             .stream()
             .collect(Collectors.toList());
+    }
+
+
+    public List<AvailableOrderDto> findTop5ByVacationIdAndOrderTypeOrderByPrice(Long id, OrderType type){
+        return jpqlQueryFactory
+                .select(Projections.fields(AvailableOrderDto.class,
+                        order.price,
+                        order.amount.sum().as("amount"),
+                        order.orderType))
+                .from(order)
+                .where( order.status.eq(ONGOING))
+                .groupBy(order.vacation, order.price)
+                .having(order.vacation.id.eq(id), order.orderType.eq(type))
+                .orderBy((type == SELL ? order.price.asc() : order.price.desc()))
+                .limit(5)
+                .fetch();
     }
 }
