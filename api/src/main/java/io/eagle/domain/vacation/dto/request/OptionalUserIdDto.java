e563a46f831594ee5a365c617c3f34696dd45ec6
NaayoungKwon <kny8092@gmail.com>
2023-03-17 12:55:27 +0900

    fix/#170 : user body 전달 삭제, query param으로 변경 일괄 적용

--- api/src/main/java/io/eagle/domain/vacation/dto/request/OptionalUserIdDto.java
+++
@@ -1,8 +0,0 @@
-package io.eagle.domain.vacation.dto.request;
-
-import lombok.Data;
-
-@Data
-public class OptionalUserIdDto {
-    Long userId;
-}
--- api/src/main/java/io/eagle/domain/vacation/dto/response/RecommendMarketDto.java
+++ api/src/main/java/io/eagle/domain/vacation/dto/response/RecommendMarketDto.java
@@ -3,9 +3,6 @@ package io.eagle.domain.vacation.dto.response;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-import java.util.List;
-
 @Data
 @NoArgsConstructor
 public class RecommendMarketDto {
