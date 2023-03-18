accea778a4f0ff6d22cb35ca5c42076e88ff408d
CEOJINSUNG <jinsung1048@gmail.com>
2023-03-18 19:22:21 +0900

    feat/#155 : RedissonClient 설정

--- core/src/main/java/io/eagle/config/RedisConfig.java
+++ core/src/main/java/io/eagle/config/RedisConfig.java
@@ -1,8 +1,8 @@
 package io.eagle.config;
 
-//import org.redisson.Redisson;
-//import org.redisson.api.RedissonClient;
-//import org.redisson.config.Config;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,7 +29,9 @@ public class RedisConfig {
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
-        config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + host + ":" + port.toString());
+        config.useSingleServer()
+            .setAddress(REDISSON_HOST_PREFIX + host + ":" + port.toString())
+            .setPassword(password);
         return Redisson.create(config);
     }
 
