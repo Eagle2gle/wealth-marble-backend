47b7b02b5fc5248143d9b8e2633ccfe72d8c4316
NaayoungKwon <kny8092@gmail.com>
2023-04-02 17:11:29 +0900

    fix/#182 : price info 생성 날짜 설정 누락되어 수정

--- batch/src/main/java/io/eagle/chunk/processor/PriceInfoProcessor.java
+++ batch/src/main/java/io/eagle/chunk/processor/PriceInfoProcessor.java
@@ -3,12 +3,16 @@ package io.eagle.chunk.processor;
 import io.eagle.domain.PriceInfoVO;
 import io.eagle.entity.PriceInfo;
 import io.eagle.entity.Vacation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.ItemProcessor;
+
+@Slf4j
 
 public class PriceInfoProcessor implements ItemProcessor<PriceInfoVO, PriceInfo> {
 
     @Override
     public PriceInfo process(PriceInfoVO lastPriceInfo) throws Exception {
+        log.debug("Vacation that traded yesterday : " + lastPriceInfo.getVacationId());
         return PriceInfo.builder()
                 .vacation(Vacation.builder().id(lastPriceInfo.getVacationId()).build())
                 .highPrice(lastPriceInfo.getHighPrice())
@@ -17,6 +21,7 @@ public class PriceInfoProcessor implements ItemProcessor<PriceInfoVO, PriceInfo>
                 .startPrice(lastPriceInfo.getStartPrice())
                 .transactionAmount(lastPriceInfo.getTransactionAmount())
                 .transactionMoney(lastPriceInfo.getTransactionMoney())
+                .createdAt(lastPriceInfo.getCreatedAt())
                 .build();
     }
 }
